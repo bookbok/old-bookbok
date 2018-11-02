2434af8bb8ecd3cc1d7659665f535dcf68e3b164
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-02 04:43:55 +0000

    Add php artisan make:model -m Book

---
+++ database/migrations/2018_11_02_133709_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('cover');
+            $table->string('author');
+            $table->unsignedInteger('genre_id');
+            $table->timestamps();
+
+            $table->foreign('genre_id')
+                ->references('id')->on('genres')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
