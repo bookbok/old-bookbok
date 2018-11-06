d814f661dc6e808cb552afd7e3fbdf4bd9fefdee
vagrant <vagrant@vagrant.vm>
2018-11-06 12:57:32 +0000

    #11 Implement BookController@index

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,8 +14,9 @@ class BookController extends Controller
      */
     public function index()
     {
-        return Book::all()->select('id','name','cover')
-                          ->toJson(JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+        return Book::orderBy('id')
+                      ->get(['id','name','cover'])
+                      ->toJson(JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
     }
 
     /**
