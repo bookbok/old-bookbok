5246c39b133f9d28ac6efa4d32a6cc3ce97b8cf4
vagrant <vagrant@vagrant.vm>
2018-11-07 07:07:50 +0000

    refactor BookListView.jsx

--- resources/js/components/BookListView.jsx
+++ resources/js/components/BookListView.jsx
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 
 export class BookListView extends Component {
-  render() {
-      const bookBody = [];
-      const bookName = [];
-      const book = [
-        {name:'c++', body:'images/book01.jpg'},
-        {name:'react01', body:'images/book02.jpg'},
-        {name:'react02', body:'images/book03.jpg'},
-      ];
+    render() {
+        const bookBody = [];
+        const bookName = [];
+        const books = [
+            {name:'c++01', body:'http://books.google.com/books/content?id=_42rGAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'},
+            {name:'c++02', body:'http://books.google.com/books/content?id=_42rGAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'},
+            {name:'c++03', body:'http://books.google.com/books/content?id=_42rGAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'},
+        ];
 
-      for (var key in book) {
-          bookBody.push(<td><img src={book[key].body} /></td>);
-          bookName.push(<th>{book[key].name}</th>);
-      }
+        books.forEach((book) => {
+           bookBody.push(<td><img src={book.body} /></td>);
+           bookName.push(<th>{book.name}</th>);
+        });
 
-      return (
-          <table border="1">
-              <tr>
-                {bookBody}
-              </tr>
-              <tr>
-                {bookName}
-              </tr>
-          </table>
-      );
-  }  
+        return (
+            <table border="1">
+               <tr>
+                 {bookBody}
+               </tr>
+               <tr>
+                 {bookName}
+               </tr>
+            </table>
+        );
+    }  
 }
