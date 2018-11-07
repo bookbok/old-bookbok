717df967607580fc3f8ee6fa06b088e711475600
kento-oka <kento-oka@kentoka.com>
2018-11-07 12:16:26 +0000

    出力範囲をコントローラー側で設定する方法に変更する

--- app/Book.php
+++ app/Book.php
@@ -6,15 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    /**
-     * 出力として出したくないフィールドのリスト
-     *
-     * @var array
-     * 
-     * @see https://readouble.com/laravel/5.7/ja/eloquent-serialization.html
-     */
-    protected $hidden = [
-        'created_at',
-        'updated_at',
-    ];
+    //
 }
