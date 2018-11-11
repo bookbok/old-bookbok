40db7fee835176e32940d7cec8c9b600ed93ecbc
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-11 10:08:53 +0000

    Add user books seeder

--- database/seeds/UserBooksTableSeeder.php
+++ database/seeds/UserBooksTableSeeder.php
@@ -11,6 +11,24 @@ class UserBooksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // HACK: BooksTableSeederで10個のBookが生成されていることを前提としている
+        App\UserBooks::create([
+            [
+                'user_id' => 1,
+                'book_id' => 1,
+            ],
+            [
+                'user_id' => 1,
+                'book_id' => 2,
+            ],
+            [
+                'user_id' => 1,
+                'book_id' => 9,
+            ],
+            [
+                'user_id' => 1,
+                'book_id' => 6,
+            ],
+        ]);
     }
 }
