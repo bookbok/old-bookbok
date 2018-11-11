41800a3d41ed8b15d2020b75675f07017e774c43
vagrant <vagrant@vagrant.vm>
2018-11-11 08:22:11 +0000

    ユーザと本のリレーション定義#37

--- app/Book.php
+++ app/Book.php
@@ -6,7 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+
+    /**
+     * リレーション定義
+     */
     public function genre(){
         return $this->belongsTo(Genre::class);
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class);
     }
 }
--- app/User.php
+++ app/User.php
@@ -27,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * リレーション定義
+     */
+    public function books(){
+        return $this->belongsToMany(Book::class);
+    }
 }
