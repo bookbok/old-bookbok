fac42c831df2982425c9c28e70abda822334339b
kento-oka <kento-oka@kentoka.com>
2018-11-13 02:47:31 +0000

    認証処理apiを追加

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
+ * Authentication Routes
+ */
+Route::post('/login','AuthenticationController@login')->name('login');
+Route::middleware('auth:api')->group(function () {
+    Route::get('/logout','AuthenticationController@logout')->name('logout');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
