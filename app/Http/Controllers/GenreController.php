b06582e4515a878595e97d4ab2cc443a6bc6f676
vagrant <vagrant@vagrant.vm>
2018-11-13 02:08:52 +0000

    ジャンルコントローラのindexメソッドの実装#39

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Genre;
 
 class GenreController extends Controller
 {
-    //
+    /**
+     * Display listing of resources.
+     * 
+     * 
+     */
+    public function index(){
+
+        $genres = Genre::orderBy('id')
+                  ->get(['id', 'name']);
+
+        return response()->json(
+            $genres,
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
 }
