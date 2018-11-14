7c4928d227d22129de033a17e3b05c76b5f19969
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-14 10:48:44 +0000

    Add review seeder relation user & book_user

---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userBooks = App\UserBook::all();
+        foreach($userBooks as $book) {
+            factory(App\Review::class)->create(['book_user_id' => $book->id, 'user_id' => $book->user_id]);
+        }
+    }
+}
