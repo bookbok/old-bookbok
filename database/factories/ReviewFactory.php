4bb809711bff27de3246e7174f79762025db2c4f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-14 11:06:46 +0000

    Update review date to 1 month ago

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,12 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 $factory->define(App\Review::class, function (Faker $faker) {
     return [
         'body' => $faker->realText($maxNbChars = 255, $indexSize = 2),
         'published_at' => $faker->dateTimeBetween($startDate = '-1 month', $endDate = 'now'),
-        'updated_at' => $faker->dateTime($max = 'now'),
-        'updated_at' => $faker->dateTime($max = 'now'),
+        'created_at' => Carbon::now()->subMonth(1),
+        'updated_at' => Carbon::now()->subMonth(1),
     ];
 });
