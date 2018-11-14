6e7b5a8370c7c0e0c0936dba26c6bf432de5c697
AKARI-I <i.akari.1122@gmail.com>
2018-11-14 07:14:37 +0000

    Add reducer 'SET_BOOK_DETAIL'

--- resources/js/reducers.js
+++ resources/js/reducers.js
@@ -1,10 +1,13 @@
 export function rootReducer(
     state = { timeLine: [] },
+    state = { bookDetail: [] },
     action
 ) {
     switch(action.type) {
         case "SET_TIMELINE":
             return { ...state, timeLine: action.timeLine };
+        case "SET_BOOK_DETAIL";
+            return { ...state, bookDetail: action.bookDetail };
     }
     return state;
 }
