0377eb970e29f2368b170b87dbd13e1827352113
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-18 12:11:12 +0000

    Refactor comment & space

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -8,12 +8,11 @@ import { BookDetailView } from './BookDetailView.jsx';
 import { BookListView } from './BookListView.jsx';
 
 
-//ヘッダー(globalnavigation）
+// bootstrap global navigation bar
 const Header = () => (
     <div>
-        {/*bootstrapナビゲーションバーの設定*/}
         <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <a class="navbar-brand"　href="#">BookBok</a>
+            <a class="navbar-brand" href="#">BookBok</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -30,6 +29,7 @@ const Header = () => (
         </nav>
     </div>
 )
+
 //react-router-dom
 export const MenuRouter = () => (
     <BrowserRouter>
