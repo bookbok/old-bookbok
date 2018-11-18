b7ac523ff10c0b34ffc72d19745741d4743659d6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-18 12:14:52 +0000

    Add url params

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import { Home } from './Home.jsx';
 import { SignUp } from './SignUp.jsx';
 import { Login } from './Login.jsx';
@@ -18,12 +18,12 @@ const Header = () => (
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="/home">ホーム <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="/">ホーム <span class="sr-only">(current)</span></a>
                     <a class="nav-item nav-link" href="/signup">新規登録</a>
                     <a class="nav-item nav-link" href="/login">ログイン</a>
                     <a class="nav-item nav-link" href="/mypage">マイページ</a>
-                    <a class="nav-item nav-link" href="/booklist">本一覧</a>
-                    <a class="nav-item nav-link" href="/bookdetailview">本詳細</a>
+                    <a class="nav-item nav-link" href="/books">本一覧</a>
+                    <a class="nav-item nav-link" href="/books/0">本詳細</a>
                 </div>
             </div>
         </nav>
@@ -35,13 +35,16 @@ export const MenuRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
-            <Route exact path="/"  />
-            <Route path="/home" component={ Home } />
-            <Route path="/signup" component={ SignUp } />
-            <Route path="/login" component={ Login } />
-            <Route path="/mypage" component={ Mypage } />
-            <Route path="/booklist" component={ BookListView } />
-            <Route path="/bookdetailview" component={ BookDetailView } />
+            <Switch>
+                <Route exact path="/" component={ Home } />
+                <Route path="/home" component={ Home } />
+                <Route path="/signup" component={ SignUp } />
+                <Route path="/login" component={ Login } />
+                <Route path="/mypage" component={ Mypage } />
+                <Route path="/books" component={ BookListView } />
+                <Route path="/books/:id" component={ BookDetailView } />
+                <Route exact component={ Home } /> {/* TODO: Replace to 404 page component*/}
+            </Switch>
         </div>
     </BrowserRouter>
 )
