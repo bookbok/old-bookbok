251f199a363e07b25dc5523bd6a71099418fe664
AKARI-I <i.akari.1122@gmail.com>
2018-11-19 05:52:45 +0000

    Fix indent

--- resources/js/actions.js
+++ resources/js/actions.js
@@ -10,12 +10,12 @@ export const fetchTimeLine = () => dispatch => {
 }
 
 export const fetchBookDetail = () => dispatch => {
-  // 本の詳細情報をどうにかして取得
-  // const bookDetail = { book_name: "book1", book_detail: "hogehoge" };
-  fetch("http://localhost:8000/api/books/1")
-    .then(res => res.json())
-    .then(json => {
+    // 本の詳細情報をどうにかして取得
+    // const bookDetail = { book_name: "book1", book_detail: "hogehoge" };
+    fetch("http://localhost:8000/api/books/1")
+     .then(res => res.json())
+     .then(json => {
       dispatch(setBookDetail(json));
-  })
+    })
 }
 
