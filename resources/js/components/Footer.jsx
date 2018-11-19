2c81a96c94991212d31cada722a0598feb90fcd6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-19 05:06:19 +0000

    Add footer component

---
+++ resources/js/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export const Footer = () => (
+    <div class="container">
+        <div class="justify-content-center">
+            <footer class="d-none d-md-block">
+                <nav>
+                    <ul class="d-flex">
+                        <li><Link to="#">利用規約</Link></li>
+                        <li><Link to="#">プライバシー</Link></li>
+                        <li><Link to="#">ヘルプ</Link></li>
+                        <li class="mr-auto"><Link to="#">お問い合わせ</Link></li>
+                        <li><Link to="#">BookBokとは</Link></li>
+                        <li><Link to="#">ユーザー</Link></li>
+                        <li><Link to="#">開発ブログ</Link></li>
+                        <li><Link to="#">ご意見</Link></li>
+                    </ul>
+                </nav>
+            </footer>
+        </div>
+    </div>
+)
