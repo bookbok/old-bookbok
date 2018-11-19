30f01518ad6264f65f50f111864ad95158553dce
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-19 16:03:26 +0000

    Update function to class

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -1,57 +1,62 @@
 import React, { Component } from 'react';
 
 //ログイン画面を表すコンポーネントを定義
-export const Login = () => (
-    <div class="container mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+export class Login extends Component {
+    render() {
+        return (
+            <div class="container mt-4">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
 
-                <div class="card">
-                    <div class="card-header">ログイン</div>
+                        <div class="card">
+                            <div class="card-header">ログイン</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="">
-                            <div class="form-group row">
-                                <label htmlFor="email" class="col-sm-4 col-form-label text-md-right">Eメール</label>
+                            <div class="card-body">
+                                <form method="POST" action="">
+                                    <div class="form-group row">
+                                        <label htmlFor="email" class="col-sm-4 col-form-label text-md-right">Eメール</label>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" required autoFocus />
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label htmlFor="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required />
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" />
-                                        <label class="form-check-label" htmlFor="remember">
-                                            ログイン状態を保持する
-                                        </label>
+                                        <div class="col-md-6">
+                                            <input id="email" type="email" class="form-control" name="email" required autoFocus />
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
 
-                            <div class="form-group row d-flex flex-column align-items-center">
-                                <button type="submit" class="btn btn-primary">
-                                    ログイン
-                                </button>
+                                    <div class="form-group row">
+                                        <label htmlFor="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
 
-                                <a class="btn btn-link" href="#">
-                                    パスワードを忘れましたか?
-                                </a>
+                                        <div class="col-md-6">
+                                            <input id="password" type="password" class="form-control" name="password" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-md-6 offset-md-4">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" />
+                                                <label class="form-check-label" htmlFor="remember">
+                                                    ログイン状態を保持する
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row d-flex flex-column align-items-center">
+                                        <button type="submit" class="btn btn-primary">
+                                            ログイン
+                                        </button>
+
+                                        <a class="btn btn-link" href="#">
+                                            パスワードを忘れましたか?
+                                        </a>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>{/* end card */}
+
                     </div>
-                </div>{/* end card */}
-
+                </div>
             </div>
-        </div>
-    </div>
-)
+        );
+    }
+
+}
