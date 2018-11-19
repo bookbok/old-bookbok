cae4a7af9793635269fde9ba82ff9afefa1419e1
AKARI-I <i.akari.1122@gmail.com>
2018-11-19 05:56:42 +0000

    Fix book reference

--- resources/js/components/BookDetailView.jsx
+++ resources/js/components/BookDetailView.jsx
@@ -10,20 +10,19 @@ export class BookDetailView extends Component {
 
     render() {
         const imgLink = 'http://books.google.com/books/content?id=_42rGAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api';
-        const bookName = this.props.bookDetail.name;
-        const bookDesc = this.props.bookDetail.description;
+        const book = this.props.bookDetail;
 
         return (
             <table border="1"><tbody>
                 <tr>
                     <td><img src='{imgLink}' /></td>
-                    <td><p>{bookName}</p></td>
+                    <td><p>{book.name}</p></td>
                 </tr>
                 <tr>
                     <td colSpan="2">概要</td>
                 </tr>
                 <tr>
-                    <td colSpan="2"><p>{bookDesc}</p></td>
+                    <td colSpan="2"><p>{book.description}</p></td>
                 </tr>
             </tbody></table>
         );
