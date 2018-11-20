1db44d8201fadb3981ddd43930fd94e16af72f6b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-20 15:07:58 +0000

    Add login action

--- resources/js/actions.js
+++ resources/js/actions.js
@@ -9,6 +9,23 @@ export const fetchTimeLine = () => dispatch => {
     dispatch(setTimeLine(timeLine));
 }
 
+// Get authentication token
+export const setAuthToken = (token) => ({ type: "SET_AUTH_TOKEN", token });
+export const requestLogin = (loginUser) => dispatch => {
+    fetch('http://localhost:8000/api/login', {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginUser)
+    })
+        .then(res => res.json())
+        .then(json => {
+            dispatch(setAuthToken(json.token));
+        });
+}
+
 export const fetchBookDetail = (id) => dispatch => {
     fetch(`http://localhost:8000/api/books/${id}`)
         .then(res => res.json())
