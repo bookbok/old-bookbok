5303673623826cbdeb68f5e41909c567de3659a4
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-20 14:25:31 +0000

    Add handles to Login components

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import { requestLogin } from "../actions.js";
+import { store } from "../index";
 
 //ログイン画面を表すコンポーネントを定義
 export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: "", password: "", remember: false };
+
+        this.submitLogin = this.submitLogin.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const name = e.target.name;
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    submitLogin(e) {
+        e.preventDefault();
+        console.log(this.state); // TODO: 消す
+        //store.dispatch(requestLogin(this.state));
+    }
+
     render() {
         return (
             <div class="container mt-4">
@@ -12,27 +36,43 @@ export class Login extends Component {
                             <div class="card-header">ログイン</div>
 
                             <div class="card-body">
-                                <form method="POST" action="">
+                                <form onSubmit={this.submitLogin}>
                                     <div class="form-group row">
                                         <label htmlFor="email" class="col-sm-4 col-form-label text-md-right">Eメール</label>
-
                                         <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control" name="email" required autoFocus />
+                                            <input id="email"
+                                                name="email"
+                                                type="email"
+                                                class="form-control"
+                                                value={this.state.email}
+                                                onChange={this.handleChange}
+                                                required
+                                                autoFocus />
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <label htmlFor="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
-
                                         <div class="col-md-6">
-                                            <input id="password" type="password" class="form-control" name="password" required />
+                                            <input id="password"
+                                                name="password"
+                                                type="password"
+                                                class="form-control"
+                                                value={this.state.password}
+                                                onChange={this.handleChange}
+                                                required />
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <div class="col-md-6 offset-md-4">
                                             <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" />
+                                                <input id="remember"
+                                                    name="remember"
+                                                    type="checkbox"
+                                                    class="form-check-input"
+                                                    checked={this.state.remmber}
+                                                    onChange={this.handleChange} />
                                                 <label class="form-check-label" htmlFor="remember">
                                                     ログイン状態を保持する
                                                 </label>
