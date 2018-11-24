7d1b683fa8acfe595bf90ca86d6993823c5bc91c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-11-24 07:07:39 +0000

    Add the remove token case

--- resources/js/reducers.js
+++ resources/js/reducers.js
@@ -5,12 +5,15 @@ export function rootReducer(
     switch(action.type) {
         case "SET_TIMELINE":
             return { ...state, timeLine: action.timeLine };
-        
+
         case "SET_GENRES":
             return { ...state, genres: action.genres };
 
         case "SET_BOOK_DETAIL":
             return { ...state, bookDetail: action.bookDetail };
+
+        case "REMOVE_AUTH_TOKEN": // ログアウトに伴い、ログイントークンを削除
+            return { ...state, token: null };
     }
     return state;
 }
