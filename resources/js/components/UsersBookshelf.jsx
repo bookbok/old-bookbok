0f1b7520c0856c8dd56c9c0edfe7ebc44c187170
AKARI-I <i.akari.1122@gmail.com>
2018-11-27 05:55:31 +0000

    Improve bookshelf view

--- resources/js/components/UsersBookshelf.jsx
+++ resources/js/components/UsersBookshelf.jsx
@@ -25,41 +25,29 @@ export class UsersBookshelf extends Component {
         {/* ユーザーが所持する本の情報を本ビューに加工 */}
         const booksInfo = usersShelf.books.map((userBook, index) => {
             return (
-                <div class="d-flex flex-column" key={index}>
+                <div class="d-inline-block" key={index}>
                     <img src={userBook.cover}/>
-                    <p>{userBook.name}</p>
+                    <pre>{userBook.name}</pre>
                 </div>
             );
         });
 
         {/* 本棚の形に加工 */}
-        const bookshelf = booksInfo.map((book, index) => {
-            if(index % 3 == 0 && booksInfo.length == (index+1)){
-                console.log("1冊目かつ最後");
-                return (
-                    <div className="d-flex" key={index}>{book}</div>
-                );
-            } else if(index % 3 == 0){
-                console.log("一番左の本");
-                return (
-                    <div className="d-flex" key={index}>{book}</div>
-                );
-            } else if(index % 3 == 2 || booksInfo.length == (index+1)){
-                console.log("一番右の本");
-                return (
-                    <div key={index}>{book}</div>
-                );
-            } else {
-                console.log("真ん中の本");
-                return (
-                    <div key={index}>{book}</div>
-                );
+        const bookshelf = [];
+        for(let i=0, key=booksInfo.length ; i<booksInfo.length; i++){
+            bookshelf.push(booksInfo[i]);
+            if(i % 3 == 2 || booksInfo.length == (i+1)){
+                bookshelf.push(<div key={key++}></div>);
             }
-        });
+        }
 
         return (
-            <div class="d-flex flex-column">
-                {bookshelf}
+            <div class="container mt-4">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        {bookshelf}
+                    </div>
+                </div>
             </div>
         );
     }
