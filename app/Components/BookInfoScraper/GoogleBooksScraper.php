a0d9273ef84902b244b7ed94f417e83eac686d8a
sugino-kazuma <vagrant@vagrant.vm>
2018-11-29 01:14:37 +0000

    リファクタリング#70

--- app/Components/BookInfoScraper/GoogleBooksScraper.php
+++ app/Components/BookInfoScraper/GoogleBooksScraper.php
@@ -7,6 +7,9 @@ use App\Book;
 
 class GoogleBooksScraper implements ScraperInterface
 {
+    // APIのエントリポイント
+    private const URI = "https://www.googleapis.com/books/v1/volumes?q=";
+
     /**
      *  コンストラクタ
      * 
@@ -19,7 +22,7 @@ class GoogleBooksScraper implements ScraperInterface
      * GoogleBooksAPIを叩き、本の情報を取得する。
      * その情報をもとにApp\Bookに情報を格納してマネージャに返す。
      * 
-     * @pram string $isbn
+     * @param string $isbn
      *  正規化されたISBN
      * 
      * @return App\Book | null
@@ -28,35 +31,31 @@ class GoogleBooksScraper implements ScraperInterface
      */
     public function searchByIsbn(string $isbn){
 
-        // APIのエントリポイント。
-        $base_url = "https://www.googleapis.com/books/v1/volumes?q=";
-
         // GuzzleHttp\Clientのインスタンスを生成。
         // 正規化されたISBN文字列をクエリパラメータとしてリクエストを送る。
         $client = new Client();
-        $response = $client->request('GET', ($base_url . $isbn))
+        $response = $client->request('GET', (self::URI . $isbn))
                            ->getBody();
 
         // JSON形式にデコード
         $bookInfo = json_decode($response);
 
-        // レスポンスがあればBookモデルを返し、なければnullを返す
-        if( $bookInfo->totalItems === 0){
-            return null;
-        } else {
-            // App\Bookのインスタンスを生成。
-            $book = new Book;
+        // レスポンスなければnullを返す
+        if( $bookInfo->totalItems === 0) return null;
 
-            // レスポンスで得た情報を該当カラムに格納する。
-            $book->isbn = $bookInfo->items[0]->volumeInfo->industryIdentifiers[1]->identifier;
-            $book->name = $bookInfo->items[0]->volumeInfo->title;
-            $book->description = $bookInfo->items[0]->volumeInfo->description;
-            $book->conver = $bookInfo->items[0]->volumeInfo->imageLinks->smallThumbnail;
-            $book->author = $bookInfo->items[0]->volumeInfo->authors;
-            // $book->genre_id = $bookInfo->items[0]->volumeInfo->categories;
+        // App\Bookのインスタンスを生成。
+        $book = new Book;
 
-            // App\Bookをスクレイプマネージャに返す
-            return $book;
-        }
+        // レスポンスで得た情報を該当カラムに格納する。
+        $book->isbn = $bookInfo->items[0]->volumeInfo->industryIdentifiers[1]->identifier;
+        $book->name = $bookInfo->items[0]->volumeInfo->title;
+        $book->description = $bookInfo->items[0]->volumeInfo->description;
+        $book->conver = $bookInfo->items[0]->volumeInfo->imageLinks->smallThumbnail;
+        $book->author = $bookInfo->items[0]->volumeInfo->authors;
+        // $book->genre_id = $bookInfo->items[0]->volumeInfo->categories;
+
+        // App\Bookをスクレイプマネージャに返す
+        return $book;
+    
     }
 }
