6d39a9a434ae93d937f144288521fd54dbae3aaa
AKARI-I <i.akari.1122@gmail.com>
2018-11-29 05:02:54 +0000

    Add bok component

--- resources/js/components/LikeBokList.jsx
+++ resources/js/components/LikeBokList.jsx
@@ -15,6 +15,29 @@ export class LikeBokList extends Component {
     };
 
     render(){
-        return (<div>LIKE BOK 一覧</div>);
+        const cover="cover.png";
+        const bookName="ゼロからわかるRuby超入門(かんたんIT基礎講座)";
+        const userName="wakamaka1122";
+        const review="めっちゃすごい";
+        const likeMark="like.png";
+        const loveMark="love.png";
+
+        return (
+            <table border="1"><tbody>
+                <tr>
+                    <td><img src={cover} /></td>
+                    <td><p>{bookName}</p></td>
+                    <td><p>{userName}</p></td>
+                </tr>
+                <tr>
+                    <td colSpan="3">{review}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><img src={likeMark} /></td>
+                    <td><img src={loveMark} /></td>
+                </tr>
+            </tbody></table>
+        );
     }
 }
