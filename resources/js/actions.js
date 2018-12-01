379cc1e55ba35173e49645f56e1c7f7a7e7c1bcb
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-01 18:29:44 +0000

    Remove mock timeline

--- resources/js/actions.js
+++ resources/js/actions.js
@@ -41,13 +41,6 @@ export function wrapAction(actionCreator, callback) {
 
 
 export const setTimeLine = timeLine => ({ type: "SET_TIMELINE", timeLine });
-export const fetchTimeLine = () => dispatch => {
-    const timeLine = [
-      { name: "bok1" },
-      { name: "bok2" },
-    ];
-    dispatch(setTimeLine(timeLine));
-}
 
 /* ==== Auth actions ==== */
 // Get authentication token
