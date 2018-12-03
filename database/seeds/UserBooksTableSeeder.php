281aa8f81395cd541646b49e0307eb5917e2badf
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-03 07:49:05 +0000

    Update user books seeder

--- database/seeds/UserBooksTableSeeder.php
+++ database/seeds/UserBooksTableSeeder.php
@@ -13,31 +13,14 @@ class UserBooksTableSeeder extends Seeder
     public function run()
     {
         // HACK: BooksTableSeederで10個のBookが生成されていることを前提としている
-        DB::table('book_user')->insert([
-            [
+        $books = App\Book::orderBy('created_at')->take(5)->get();
+        foreach($books as $book) {
+            App\BookUser::create([
                 'user_id' => 1,
-                'book_id' => 1,
+                'book_id' => $book->isbn,
                 'created_at' => Carbon::now()->subDays(15),
                 'updated_at' => Carbon::now()->subDays(15),
-            ],
-            [
-                'user_id' => 1,
-                'book_id' => 2,
-                'created_at' => Carbon::now()->subDays(10),
-                'updated_at' => Carbon::now()->subDays(10),
-            ],
-            [
-                'user_id' => 1,
-                'book_id' => 9,
-                'created_at' => Carbon::now()->subDays(4),
-                'updated_at' => Carbon::now()->subDays(4),
-            ],
-            [
-                'user_id' => 1,
-                'book_id' => 6,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-        ]);
+            ]);
+        }
     }
 }
