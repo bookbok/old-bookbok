d5a938d9b301357d2f13f6a2378d550cdf25a66e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-07 02:25:01 +0000

    Add relation

--- app/Reaction.php
+++ app/Reaction.php
@@ -9,4 +9,13 @@ class Reaction extends Model
     protected $fillable = [
         'user_id', 'bok_id', 'liked', 'loved',
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bok() {
+        return $this->belongsTo(Bok::class);
+    }
+
 }
