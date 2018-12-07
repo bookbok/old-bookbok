d5ec35e8b717f6752991e9bc59dc01cf9453353a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-07 02:24:41 +0000

    Add sql query logger

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            $sql = $query->sql;
+            foreach($query->bindings as $bind) {
+                $sql = preg_replace("/\?/", $bind, $sql, 1);
+            }
+            logger($sql);
+        });
     }
 
     /**
