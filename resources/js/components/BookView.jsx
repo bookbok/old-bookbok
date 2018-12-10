006a32a33937cc53cf712fe2b233f4724f76b8d0
vagrant <vagrant@vagrant.vm>
2018-12-10 05:45:03 +0000

    refactor BookView.jsx & _booklist_view.scss

--- resources/js/components/BookView.jsx
+++ resources/js/components/BookView.jsx
@@ -3,22 +3,22 @@ import React, { Component } from 'react';
 export class BookView extends Component {
     render() {
         const book = this.props.book;
-        const bookName = [];
+        let bookName;
         
         if(book.name != "") {
             const width = screen.width <= 750 ? 12 : 14;
             if(book.name.charAt(width+1) != "") {
-                bookName.push(<pre>{book.name.slice(0, width)}<br/>{book.name.slice(width)}</pre>);
+                bookName = <pre className="book-view-pre">{book.name.slice(0, width)}<br/>{book.name.slice(width)}</pre>;
             } else {
-                bookName.push(<pre>{book.name.slice(0, width)}<br/><br/></pre>);
+                bookName = <pre className="book-view-pre">{book.name.slice(0, width)}<br/><br/></pre>;
             }
         } else {
-                bookName.push(<pre><br/><br/></pre>);
+                bookName = <pre className="book-view-pre"><br/><br/></pre>;
         }
 
         return (
             <div className="d-inline-block col-md-4 col-4"> 
-                <img src={book.cover}/>
+                <img className="book-view-img" src={book.cover}/>
                 {bookName}
                 <br/>
             </div>
