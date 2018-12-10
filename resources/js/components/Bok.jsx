5141d72d190bd40965fdeabb2b7c88443df6ea38
AKARI-I <i.akari.1122@gmail.com>
2018-12-10 06:45:51 +0000

    Update change click flg

--- resources/js/components/Bok.jsx
+++ resources/js/components/Bok.jsx
@@ -8,6 +8,34 @@ export class Bok extends Component {
 
         this.likeClass = "like";
         this.loveClass = "love";
+        this.likeFlg = false;
+        this.loveFlg = false;
+        this.clickLike = this.clickLike.bind(this);
+        this.clickLove = this.clickLove.bind(this);
+    }
+
+    clickLike(e){
+        if(!this.likeFlg){
+            this.likeClass += " liked";
+            this.likeFlg = true;
+        } else {
+            this.likeClass = "like";
+            this.likeFlg = false;
+        }
+        console.log("likeClass : " + this.likeClass);
+        console.log("likeFlg : " + this.likeFlg);
+    }
+
+    clickLove(e){
+        if(!this.loveFlg){
+            this.loveClass += " loved";
+            this.loveFlg = true;
+        } else {
+            this.loveClass = "love";
+            this.loveFlg = false;
+        }
+        console.log("loveClass : " + this.loveClass);
+        console.log("loveFlg : " + this.loveFlg);
     }
 
     render(){
@@ -15,14 +43,6 @@ export class Bok extends Component {
         let page = "p" + likeBok.page_num_begin;
         if(likeBok.page_num_begin !== likeBok.page_num_end){
             page += (" ~ p" + likeBok.page_num_end);
-        }
-
-        const clickLike = (e) => {
-            console.log("like!");
-        }
-
-        const clickLove = (e) => {
-            console.log("love!");
         }
 
         return (
@@ -41,8 +61,8 @@ export class Bok extends Component {
                             <tr><td colSpan="3">{likeBok.body}</td></tr>
                             <tr>
                                 <td>{likeBok.created_at}</td>
-                                <td><div className={this.likeClass} onClick={clickLike}/></td>
-                                <td><div className={this.loveClass} onClick={clickLove}/></td>
+                                <td><div className={this.likeClass} onClick={this.clickLike}/></td>
+                                <td><div className={this.loveClass} onClick={this.clickLove}/></td>
                             </tr>
                         </tbody></table>
                     </td>
