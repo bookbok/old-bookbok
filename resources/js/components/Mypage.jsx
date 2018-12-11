d59a58d104a1bb07977194392a0fa4458d3a84eb
Kazuki <kd1210584@st.kobedenshi.ac.jp>
2018-12-11 05:26:57 +0000

    display tabs

--- resources/js/components/Mypage.jsx
+++ resources/js/components/Mypage.jsx
@@ -5,6 +5,27 @@ import { ConnectedUserInfo } from "../containers.js";
 export const Mypage = () => (
     <div>
         <h1>マイページ</h1>
+    
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active">トップ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">本棚</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">フォロー</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">フォロワー</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">LIKE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">LOVE</a>
+            </li>
+        </ul>
     </div>
 )
 
