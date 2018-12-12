8d97dfbca7d19752d873ebe33cbba5013f68eccb
Kazuki <kd1210584@st.kobedenshi.ac.jp>
2018-12-12 04:06:58 +0000

    switching_tabs

--- resources/js/components/Mypage.jsx
+++ resources/js/components/Mypage.jsx
@@ -5,27 +5,41 @@ import { ConnectedUserInfo } from "../containers.js";
 export const Mypage = () => (
     <div>
         <h1>マイページ</h1>
-    
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-                <a class="nav-link active">トップ</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">本棚</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">フォロー</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">フォロワー</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">LIKE</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">LOVE</a>
-            </li>
-        </ul>
+        
+        <div class="p-3">
+
+            {/*タブのボタン部分*/}
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a href="#top" class="nav-link active" data-toggle="tab">トップ</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#user_books" class="nav-link" data-toggle="tab">本棚</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#likes" class="nav-link" data-toggle="tab">LIKE</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#loves" class="nav-link" data-toggle="tab">LOVE</a>
+                </li>
+            </ul>
+
+            {/*タブのコンテンツ部分*/}
+            <div class="tab-content">
+                <div id="top" class="tab-pane active">
+                    トップ
+                </div>
+                <div id="user_books" class="tab-pane">
+                    本棚
+                </div>
+                <div id="likes" class="tab-pane">
+                    LIKE
+                </div>
+                <div id="loves" class="tab-pane">
+                    LOVE
+                </div>
+            </div>
+        </div>
     </div>
 )
 
