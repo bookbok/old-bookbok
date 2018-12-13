0a3ddd77594d62a2909c08cb7ccfd7fc682a382f
Kazuki <kd1210584@st.kobedenshi.ac.jp>
2018-12-13 05:25:26 +0000

    add form

--- resources/js/components/Contribution.jsx
+++ resources/js/components/Contribution.jsx
@@ -8,7 +8,7 @@ export const Contribution = () => (
         </button>
 
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalCenterTitle">タイトル</h5>
@@ -17,7 +17,24 @@ export const Contribution = () => (
                         </button>
                     </div>
                     <div class="modal-body">
-                        <p>コンテンツ文。</p>
+                        <form>
+                            <div class="row">
+                                <div class="col">
+                                    <input type="number" class="form-control" placeholder="開始ページ" />
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" placeholder="終了ページ" />
+                                </div>
+                            </div>
+                            <br />
+                            <div class="form-group">
+                                <input type="number" class="form-control" placeholder="該当行番号" />
+                            </div>
+                            <div class="form-group">
+                                <label for="impressions-text">感想:</label>
+                                <textarea class="form-control" id="impressions-text"></textarea>
+                            </div>
+                        </form>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
