476c1ae60b3915ae90c7c9fb01bb1295b84f9d68
kento-oka <kento-oka@kentoka.com>
2018-12-13 05:47:24 +0000

    パスワードリセットコントローラーを作成

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,37 +3,108 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Contracts\Auth\PasswordBroker;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
-
-    use ResetsPasswords;
-
+    const ERROR_MSG = [
+        PasswordBroker::INVALID_USER     => '',
+        PasswordBroker::INVALID_PASSWORD => '',
+        PasswordBroker::
+    ];
     /**
-     * Where to redirect users after resetting their password.
+     * パスワードリセットメール送信処理
      *
-     * @var string
+     * @param   Request    $request
+     *  リクエスト
+     * 
+     * @return  \Illuminate\Http\Response
      */
-    protected $redirectTo = '/home';
+    public function send(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $response = Password::broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return response()->json([
+            'message' => 'We successfully sent a mail with a link to the password reset page!',
+        ], 200);
+    }
 
     /**
-     * Create a new controller instance.
+     * パスワードリセット処理
      *
+     * @param   Request    $request
+     *  リクエスト
+     * 
+     * @return  \Illuminate\Http\Response
+     */
+    public function reset(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'token'    => 'required',
+            'email'    => 'required|email|max:255',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+   
+        $response = Password::broker()->reset(
+            $request->only('token', 'email', 'password') + [
+                'password_confirmation' => $request->all()['password'],
+            ],
+            [$this, 'resetPassword']
+        );
+
+        if (Password::PASSWORD_RESET !== $response) {
+            return response()->json([
+                'message' => self::ERROR_MSG[$response] ?? 'Error...',
+            ], 400);
+        }
+        
+        return response()->json([
+            'message' => 'You have successfully changed password!',
+        ], 200);
+    }
+
+    /**
+     * パスワードをリセットする
+     * 
+     * @param   \App\User   $user
+     *  ユーザー
+     * @param   string  $password
+     *  パスワード
+     * 
      * @return void
      */
-    public function __construct()
+    public function resetPassword(\App\User $user, string $password)
     {
-        $this->middleware('guest');
+        $user->password = password_hash($password, PASSWORD_DEFAULT);
+
+        // これは必要ないけど念のため
+        $user->setRememberToken(Str::random(60));
+
+        // アクセストークンをすべて無効化
+        foreach($user->tokens as $token) {
+            $token->revoke();   
+        }
+
+        $user->save();
+
+        event(new PasswordReset($user));
     }
 }
