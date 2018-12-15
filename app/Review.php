29595749b29d527ae78fdc0c10a69c354811636a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-15 19:51:52 +0000

    Add fillable to Review

--- app/Review.php
+++ app/Review.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $fillable = [
+        'user_id', 'user_book_id', 'body', 'published_at',
+    ];
+
     /**
      *リレーション定義
      */
