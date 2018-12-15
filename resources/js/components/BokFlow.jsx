d475a967d665779d3ebbdce69147aef68c5f065f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-15 21:30:21 +0000

    Add bok flow none pattern

--- resources/js/components/BokFlow.jsx
+++ resources/js/components/BokFlow.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import { fetchBokFlow } from "../actions";
 import { store } from "../store";
 import { isEmpty } from "../utils.js";
 import { Loading } from "./shared/Loading";
 import { Bok } from "./Bok.jsx";
 
 export class BokFlow extends Component {
+    componentDidMount() {
+        store.dispatch(fetchBokFlow());
+    };
+
     render() {
         if(isEmpty(this.props.bokFlow)){
             return <Loading />;
+        } else if(typeof this.props.bokFlow === 'string') {
+            return (
+                <div className="container mt-4">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8 main-content p-5">
+                            <div>{this.props.bokFlow}</div>
+                        </div>
+                    </div>
+                </div>
+            );
         }
 
         const bokFlow = this.props.bokFlow.map(bok => {
