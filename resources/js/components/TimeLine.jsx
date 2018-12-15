c13272757837414925be4c1e06166eba069d1da0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-15 21:15:36 +0000

    Update bok flow

--- resources/js/components/TimeLine.jsx
+++ resources/js/components/TimeLine.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
+import { store } from "../store";
+import { isEmpty } from "../utils.js";
+import { Loading } from "./shared/Loading";
+import { Bok } from "./Bok.jsx";
 
-export class TimeLine extends Component {
+export class BokFlow extends Component {
     render() {
-        if(this.props.timeLine == []){
-            return;
+        if(isEmpty(this.props.bokFlow)){
+            return <Loading />;
         }
-        const timeLine = this.props.timeLine.map((v, i) => (
-            <li key={i}>
-                {v.name}
-            </li>
-        ));
+
+        const bokFlow = this.props.bokFlow.map(bok => {
+            return <div className="mt-2" key={i}><Bok bok={bok}/></div>
+        });
+
         return (
-            <div>
-                <ul>{timeLine}</ul>
+            <div className="container mt-4">
+                <div className="row justify-content-center">
+                    <div className="col-md-8 main-content p-5">
+                        {bokFlow}
+                    </div>
+                </div>
             </div>
         );
     }
