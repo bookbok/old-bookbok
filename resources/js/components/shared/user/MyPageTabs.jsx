d9f46306101172067f824cf7bc7a5de6ad5487f3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-17 06:41:35 +0000

    Update a to Link

--- resources/js/components/shared/user/MyPageTabs.jsx
+++ resources/js/components/shared/user/MyPageTabs.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export class MyPageTabs extends Component {
     render() {
@@ -9,24 +10,24 @@ export class MyPageTabs extends Component {
                 {/*タブのボタン部分*/}
                 <ul className="nav nav-tabs">
                     <li className="nav-item">
-                        <a href={`/users/${userId}/`}
+                        <Link to={`/users/${userId}/`}
                            className={this.props.isTop ? 'nav-link active' : 'nav-link'}
-                           data-toggle={this.props.isTop ? 'tab' : ''}>トップ</a>
+                           data-toggle={this.props.isTop ? 'tab' : ''}>トップ</Link>
                     </li>
                     <li className="nav-item">
-                        <a href={`/users/${userId}/user_books`}
+                        <Link to={`/users/${userId}/user_books`}
                            className={this.props.isUserBooks ? 'nav-link active' : 'nav-link'}
-                           data-toggle={this.props.isUserBooks ? 'tab' : ''}>本棚</a>
+                           data-toggle={this.props.isUserBooks ? 'tab' : ''}>本棚</Link>
                     </li>
                     <li className="nav-item">
-                        <a href={`/users/${userId}/likes`}
+                        <Link to={`/users/${userId}/likes`}
                            className={this.props.isLikes ? 'nav-link active' : 'nav-link'}
-                           data-toggle={this.props.isLikes ? 'tab' : ''}>LIKE</a>
+                           data-toggle={this.props.isLikes ? 'tab' : ''}>LIKE</Link>
                     </li>
                     <li className="nav-item">
-                        <a href={`/users/${userId}/loves`}
+                        <Link to={`/users/${userId}/loves`}
                            className={this.props.isLoves ? 'nav-link active' : 'nav-link'}
-                           data-toggle={this.props.isLoves ? 'tab' : ''}>LOVE</a>
+                           data-toggle={this.props.isLoves ? 'tab' : ''}>LOVE</Link>
                     </li>
                 </ul>
             </div>
