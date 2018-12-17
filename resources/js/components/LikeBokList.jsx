f7cd8da58fa8ce54f75b5fffb01fbe227130a198
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-17 11:51:53 +0000

    Add user info to likes bok list

--- resources/js/components/LikeBokList.jsx
+++ resources/js/components/LikeBokList.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
-import { fetchLikeBoks } from "../actions.js";
+import { fetchLikeBoks, fetchUser } from "../actions";
 import { store } from "../store";
-import { isEmpty } from "../utils.js";
+import { isEmpty } from "../utils";
+
 import { Loading } from "./shared/Loading";
-import { Bok } from "./Bok.jsx";
+import { Bok } from "./Bok";
 import { MyPageTabs } from "./shared/user/MyPageTabs";
+import { FloatUserInfo } from "./shared/user/FloatUserInfo";
 
 export class LikeBokList extends Component {
     componentDidMount(){
         const userId = parseInt(this.props.match.params.id);
         store.dispatch(fetchLikeBoks(userId));
+        store.dispatch(fetchUser(userId));
     };
 
     render(){
         const likeBoks = this.props.likeBoks;
-
-        if(isEmpty(likeBoks)){
+        const user = this.props.user;
+        if(isEmpty(likeBoks) || isEmpty(user)){
             return <Loading />;
         }
 
@@ -24,14 +27,18 @@ export class LikeBokList extends Component {
         })
 
         return (
-            <div className="container mt-4">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <MyPageTabs isLikes userId={this.props.match.params.id} />
-                        <div className="mt-4">
-                            <p>LikeBok一覧</p>
-                            {boks}
-                            <br/>
+            <div className="page-content-wrap row">
+                <FloatUserInfo user={user} />
+
+                <div className="container mt-4">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8 main-content p-5">
+                            <MyPageTabs isLikes userId={this.props.match.params.id} />
+                            <div className="mt-4">
+                                <p>LikeBok一覧</p>
+                                {boks}
+                                <br/>
+                            </div>
                         </div>
                     </div>
                 </div>
