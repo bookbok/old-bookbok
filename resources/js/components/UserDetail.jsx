16ea9c0e10396d86e28f5aa2d94f1e649ce4ec92
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-17 11:52:40 +0000

    Add user info to user detail

--- resources/js/components/UserDetail.jsx
+++ resources/js/components/UserDetail.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { store } from "../store";
+import { fetchUser } from "../actions";
+import { isEmpty } from "../utils";
 
-import { ConnectedUserInfo } from "../containers.js";
+import { Loading } from "./shared/Loading";
 import { MyPageTabs } from "./shared/user/MyPageTabs";
+import { FloatUserInfo } from "./shared/user/FloatUserInfo";
 
 //マイページ画面を表すコンポーネントを定義
 export class UserDetail extends Component {
+    componentDidMount() {
+        store.dispatch(fetchUser(this.props.match.params.id));
+    }
     render() {
+        const user = this.props.user;
+        if(isEmpty(user)){
+            return <Loading />;
+        }
+
         return (
-            <div className="container mt-4">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <MyPageTabs isTop userId={this.props.match.params.id} />
-                        <div className="mt-4">
-                            <h1>トップ</h1>
+            <div className="page-content-wrap row">
+                <FloatUserInfo user={user} />
+
+                <div className="container mt-4">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8 main-content p-5">
+                            <MyPageTabs isTop userId={this.props.match.params.id} />
+                            <div className="mt-4">
+                                <h1>トップ</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
