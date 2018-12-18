52000876432972c1999389acdb07e74a362cc271
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2018-12-18 05:27:53 +0000

    fixed BookView.jsx

--- resources/js/components/BookView.jsx
+++ resources/js/components/BookView.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { mb_substr, ja2Bit } from '../utils.js';
 
 export class BookView extends Component {
     render() {
         const book = this.props.book;
         let bookName;
 
+        for (var i = 0, jaCnt = 0 ; i < book.name.length; i++) {
+            if (ja2Bit(book.name.charAt(i))) {
+                jaCnt++;
+            }
+        }
+             
         if(book.name != "") {
-            const width = screen.width <= 750 ? 12 : 14;
-            if(book.name.charAt(width+1) != "") {
-                bookName = <pre className="book-view-pre">{book.name.slice(0, width)}<br/>{book.name.slice(width)}</pre>;
+            const width = jaCnt < 9 ? 13 : 9;
+            const device = screen.width <= 750 ? -2 : 0;
+            if(book.name.charAt((width+device)+1) != "") {
+                bookName = <pre className="book-view-pre">{mb_substr(book.name, 0, width+device)}<br/>{mb_substr(book.name, width+device, book.name.length)}</pre>;
             } else {
-                bookName = <pre className="book-view-pre">{book.name.slice(0, width)}<br/><br/></pre>;
+                bookName = <pre className="book-view-pre">{mb_substr(book.name, 0, book.name.length)}<br/><br/></pre>;
             }
         } else {
                 bookName = <pre className="book-view-pre"><br/><br/></pre>;
--- resources/js/utils.js
+++ resources/js/utils.js
@@ -17,3 +17,29 @@ export function convertQuery(obj) {
         return key + "=" + body[key];
     }).join('&')
 }
+
+$.isSurrogatePear = function(upper, lower) {
+      return 0xD800 <= upper && upper <= 0xDBFF && 0xDC00 <= lower && lower <= 0xDFFF;
+};
+//マルチバイト文字対応型の文字列の切り出しをする
+export function mb_substr(str, begin, end) {
+    let ret = '';
+    for (let i = 0, len = 0; i < str.length; i++, len++) {
+        let upper = str.charCodeAt(i);
+        let lower = str.length > (i + 1) ? str.charCodeAt(i + 1) : 0;
+        let string = '';
+        if ($.isSurrogatePear(upper, lower)) {
+            i++;
+            string = String.fromCharCode(upper, lower);
+        } else {
+            string = String.fromCharCode(upper);
+        }
+        if(begin <= len && len < end) { ret += string; }
+    }
+    return ret;
+}
+
+//漢字・ひらがな・カタカナ以外の文字を含んでいたら「false」が返る
+export function ja2Bit(chr) {
+    return ( chr.match(/^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/) )? true : false
+}
