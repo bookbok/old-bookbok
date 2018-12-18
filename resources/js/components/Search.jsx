51fdf917dc25fe0eb18bde6041a77ddbaea052d4
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-18 05:23:11 +0000

    Update search button view

--- resources/js/components/Search.jsx
+++ resources/js/components/Search.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Search = () => (
-    <div>
-        <div className="form-group">
-            <div className="row">
-                <div className="col-xs-4">
-                    <input type="text" className="form-control" placeholder="テキスト入力欄" />
+export class Search extends Component {
+    render() {
+        return (
+            <div className="form-group">
+                <div className="row">
+                    <div className="col-xs-4">
+                        <input type="text" className="form-control" placeholder="テキスト入力欄" />
+                    </div>
+                    <span className="input-group-btn">
+                        <button type="button" className="btn btn-light border">
+                            <i className="fas fa-search"></i> 検索
+                        </button>
+                    </span>
                 </div>
-                <span className="input-group-btn">
-                    <button type="button" className="btn btn-default">検索</button>
-                </span>
             </div>
-        </div>
-    </div>
-
-)
+        );
+    }
+}
