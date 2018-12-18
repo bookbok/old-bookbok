cb4e6fd985f7c4fd25f51b996bdb0aafcfe8ef70
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-18 10:39:29 +0000

    Add isbn submit to server

--- resources/js/components/shared/book/ISBNModal.jsx
+++ resources/js/components/shared/book/ISBNModal.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
+import { storeISBNToUserBookDirect } from "../../../actions";
 
 export class ISBNModal extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { isbn: null };
+        this.state = { isbn: "", isInvalid: false };
         this.handleChangeISBN = this.handleChangeISBN.bind(this);
+        this.handleRegisterISBN = this.handleRegisterISBN.bind(this);
     }
 
     componentDidMount() {
@@ -18,12 +20,15 @@ export class ISBNModal extends Component {
 
     handleChangeISBN(e) {
         this.setState({ isbn: e.target.value });
-        ReactDOM.findDOMNode(this.refs.isbn).value = e.target.value;
     }
 
     handleRegisterISBN(e) {
         e.preventDefault();
-
+        storeISBNToUserBookDirect(1, this.state.isbn).then(res => {
+            this.props.history.push(`/users/${res.user.id}/user_books/${res.id}`);
+        }).catch(err => {
+            this.setState({ isInvalid: true });
+        });
     }
 
     render() {
@@ -43,26 +48,28 @@ export class ISBNModal extends Component {
                                 </button>
                             </div>
 
-                            <div className="modal-body">
-                                <input id="inputISBN"
-                                    type="number"
-                                    min="0"
-                                    max="9999999999999"
-                                    className="form-control form-control-lg"
-                                    placeholder="9784041026168"
-                                    onChange={this.handleChangeISBN}
-                                    ref="isbn"
-                                    required />
-                            </div>
+                            <form onSubmit={this.handleRegisterISBN}>
+                                <div className="modal-body">
+                                    <input id="inputISBN"
+                                        type="number"
+                                        min="0"
+                                        max="9999999999999"
+                                        className={`form-control form-control-lg ${this.state.isInvalid && 'is-invalid'}`}
+                                        placeholder="9784041026168"
+                                        value={this.state.isbn}
+                                        onChange={this.handleChangeISBN}
+                                        ref="isbn"
+                                        required />
+                                    <div className="invalid-feedback">
+                                        入力されたISBNが存在しません
+                                    </div>
+                                </div>
 
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                                <button type="button"
-                                    className="btn btn-success"
-                                    onClick={this.handleRegisterISBN}>
-                                    登録
-                                </button>
-                            </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                    <button type="submit" className="btn btn-success">登録</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
