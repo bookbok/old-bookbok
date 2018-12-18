7316c191bd6bd030f3997ac8415a859b5c478113
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-18 00:19:19 +0000

    Fix

--- resources/js/components/Contribution.jsx
+++ resources/js/components/Contribution.jsx
@@ -3,44 +3,44 @@ import React from 'react';
 export const Contribution = () => (
 
     <div>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
             BOKボタン
         </button>
 
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenterTitle">タイトル</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+        <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalCenterTitle">タイトル</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="閉じる">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
+                    <div className="modal-body">
                         <form>
-                            <div class="row">
-                                <div class="col">
-                                    <input type="number" class="form-control" placeholder="開始ページ" />
+                            <div className="row">
+                                <div className="col">
+                                    <input type="number" className="form-control" placeholder="開始ページ" />
                                 </div>
-                                <div class="col">
-                                    <input type="number" class="form-control" placeholder="終了ページ" />
+                                <div className="col">
+                                    <input type="number" className="form-control" placeholder="終了ページ" />
                                 </div>
-                                <div class="col">
-                                    <input type="number" class="form-control" placeholder="該当行番号" />
+                                <div className="col">
+                                    <input type="number" className="form-control" placeholder="該当行番号" />
                                 </div>
                             </div>
                             <br />
-                            <div class="form-group">
-                                <label for="impressions-text" class="control-label">感想&nbsp;
-                                        <span class="badge badge-danger">必須</span>
+                            <div className="form-group">
+                                <label htmlFor="impressions-text" className="control-label">感想&nbsp;
+                                        <span className="badge badge-danger">必須</span>
                                 </label>
-                                <textarea class="form-control" id="impressions-text" required></textarea>
+                                <textarea className="form-control" id="impressions-text" required></textarea>
                             </div>
                         </form>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                        <button type="submit" class="btn btn-primary">BOKを投稿</button>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                        <button type="submit" className="btn btn-primary">BOKを投稿</button>
                     </div>
                 </div>
             </div>
