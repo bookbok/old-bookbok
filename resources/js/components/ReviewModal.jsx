5ab84d382ecbdcabad555f01ea2a405dcc5bf3b9
Kazuki <kd1210584@st.kobedenshi.ac.jp>
2018-12-19 01:11:33 +0000

    Update class

--- resources/js/components/ReviewModal.jsx
+++ resources/js/components/ReviewModal.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 
-export const ReviewModal = () => (
+export class ReviewModal extends Component {
+/*    constructor(props) {
+        super(props);
 
-    <div>
-        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#ReviewModalCenter">
-            レビューを投稿する
-        </button>
+        this.state = { title: "", body: "" };
 
-        <div className="modal fade" id="ReviewModalCenter" tabIndex="-1" role="dialog" aria-labelledby="ReviewModalCenterTitle" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal" aria-label="閉じる">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <form>
-                            <div className="row">
-                                <div className="col">レビューのタイトルを記入してください
-                                    <input type="text" className="form-control" placeholder="この本をズバリ一言で！" />
-                                </div>
+        this.handleChange = this.handleChange.bind(this);
+    }
+  */  
+ 
+    render() {
+        return (
+            <div>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#ReviewModalCenter">
+                    レビューを投稿する
+                </button>
+
+                <div className="modal fade" id="ReviewModalCenter" tabIndex="-1" role="dialog" aria-labelledby="ReviewModalCenterTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal" aria-label="閉じる">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
-                            <div className="form-group" className="mt-5">
-                                <label htmlFor="impressions-text" className="control-label">ここにレビューを記入してください&nbsp;
-                                        <span className="badge badge-danger">必須</span>
-                                </label>
-                                <textarea className="form-control" id="impressions-text" required placeholder="1冊を読み終えてどうだったか。これから読む人に伝えたいこと。"></textarea>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="row">
+                                        <div className="col">レビューのタイトルを記入してください
+                                            <input type="text" className="form-control" placeholder="この本をズバリ一言で！" />
+                                        </div>
+                                    </div>
+                                    <div className="form-group" className="mt-5">
+                                        <label htmlFor="impressions-text" className="control-label">ここにレビューを記入してください&nbsp;
+                                                <span className="badge badge-danger">必須</span>
+                                        </label>
+                                        <textarea className="form-control" id="impressions-text" required placeholder="1冊を読み終えてどうだったか。これから読む人に伝えたいこと。"></textarea>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                        <button type="submit" className="btn btn-primary">レビューを投稿</button>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                <button type="submit" className="btn btn-primary">レビューを投稿</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-
-)
+        );
+    }
+}
