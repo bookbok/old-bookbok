703cb05aa68fb6a9c748498e803f1991b9c8c9b6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-19 02:47:28 +0000

    Refactor tag

--- resources/js/components/Search.jsx
+++ resources/js/components/Search.jsx
@@ -7,7 +7,7 @@ export class Search extends Component {
                 <input type="search" className="form-control" placeholder="テキスト入力欄" />
                 <span className="input-group-btn">
                     <button type="button" className="btn btn-light border">
-                        <i className="fas fa-search"></i><span className="search-button-text"> 検索</span>
+                        <i className="fas fa-search" /><span className="search-button-text"> 検索</span>
                     </button>
                 </span>
             </div>
