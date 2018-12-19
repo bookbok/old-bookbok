93ebb68057c0f3656251ad6666ca3a6e911451af
kento-oka <kento-oka@kentoka.com>
2018-12-19 10:08:23 +0900

    デフォルトでネタバレとするジャンルのIDリスト定数を追加

--- app/Genre.php
+++ app/Genre.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
+    public const SPOILER_ID_LIST = [1, 18];
+
     public function books(){
         return $this->hasMany(Book::class);
     }
