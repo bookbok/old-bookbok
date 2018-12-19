98a053ad8b15eb573881bccae886a556f6966c76
sugino-kazuma <vagrant@vagrant.vm>
2018-12-19 23:51:51 +0000

    見た目の修正#259

--- app/Http/Controllers/UserBookController.php
+++ app/Http/Controllers/UserBookController.php
@@ -52,9 +52,11 @@ class UserBookController extends Controller
         // 認可チェック
         $authId = Auth::id();
         if($authId != $userId){
-            return response()->json([
-                'status' => 403,
-                'userMessage' => '自分以外の本棚に追加することはできません。'],
+            return response()->json(
+                [
+                    'status' => 403,
+                    'userMessage' => '自分以外の本棚に追加することはできません。'
+                ],
                 403,
                 [],
                 JSON_UNESCAPED_UNICODE
@@ -88,9 +90,11 @@ class UserBookController extends Controller
 
             // すべてのScraperが情報取得に失敗した場合
             if($new_book == null){
-                return response()->json([
-                        'status' => 500,
-                        'userMessage' => 'お探しの本の情報を取得することができませんでした。'],
+                return response()->json(
+                        [
+                            'status' => 500,
+                            'userMessage' => 'お探しの本の情報を取得することができませんでした。'
+                        ],
                         500,
                         [],
                         JSON_UNESCAPED_UNICODE
@@ -103,9 +107,11 @@ class UserBookController extends Controller
         // 当該ユーザのuser_bookテーブルに同じ本がすでに登録されているかのチェック
         $is_userBook_exists = UserBook::where('book_id', $book->id)->where('user_id', $userId)->first();
         if($is_userBook_exists){
-            return response()->json([
-                'status' => 400,
-                'userMessage' => '追加しようとした本はすでに本棚に登録されています。'],
+            return response()->json(
+                [
+                    'status' => 400,
+                    'userMessage' => '追加しようとした本はすでに本棚に登録されています。'
+                ],
                 400,
                 [],
                 JSON_UNESCAPED_UNICODE);
