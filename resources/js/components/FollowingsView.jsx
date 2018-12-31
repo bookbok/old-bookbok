970c5cebf34c313c18a49cec3d59f7994b21d565
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-12-31 17:06:51 +0000

    Add followings component

--- resources/js/components/FollowingsView.jsx
+++ resources/js/components/FollowingsView.jsx
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { store } from "../store";
+import { fetchUser, fetchFollowings } from "../actions";
+import { isEmpty } from "../utils";
 
 import { Loading } from "./shared/Loading";
+import { MyPageTabs } from "./shared/user/MyPageTabs";
+import { FloatUserInfo } from "./shared/user/FloatUserInfo";
+import { Link } from 'react-router-dom';
+import SimpleUser from './shared/user/SimpleUser';
 
 class FollowingsView extends Component {
+    componentDidMount(){
+        const userId = parseInt(this.props.match.params.id);
+        store.dispatch(fetchFollowings(userId));
+        store.dispatch(fetchUser(userId));
+    }
+
     render() {
+        const followings = this.props.followings;
+        const user = this.props.user;
+        if(isEmpty(followings) || isEmpty(user)){
+            return <Loading />;
+        }
+
+        const bindedUsers = followings.map((user, i) => (
+            <SimpleUser user={user} key={i} />
+        ));
+
         return (
-            <div>followings</div>
+            <div className="page-content-wrap row">
+                <FloatUserInfo user={user} />
+
+                <div className="container mt-4">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8 main-content p-5">
+                            <MyPageTabs userId={this.props.match.params.id} />
+                            <div className="mt-4">
+                                <p>フォロワー一覧</p>
+                                {bindedUsers}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
