80c636e5455d00844246fc5d67299a53978e57ef
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-08 15:20:00 +0000

    Add ptototype home page

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 //ホーム画面を表すコンポーネントを定義
 export const Home = () => (
@@ -8,31 +9,76 @@ export const Home = () => (
                 <h1>読書を今よりも素敵に。</h1>
                 <p>本と感想をまとめて管理できるサービスです。</p>
                 <p>他人の書いた感想を読んだり、読書管理をしましょう。</p>
+
+                <div className="register-wrapper mt-5">
+                    <Link to="/register" className="btn btn-success">BookBokを始める</Link>
+                </div>
             </div>
         </div>
 
         <div className="services-wrapper">
             <div className="container">
-                <div className="services">
+                <div className="service-heading">
+                    <h2>BookBokには様々な仕組みがあります</h2>
+                </div>
 
+                <div className="services">
                     <div className="service-box">
                         <div className="service-icon">
-                            <i className="fa fa-4x fa-tags" />
+                            <i className="fas fa-4x fa-book" />
                             <p>本棚</p>
                         </div>
-                        <p>持っている本を本棚で一括管理</p>
+                        <p className="service-text">持っている本を本棚で一括管理</p>
                     </div>
 
                     <div className="service-box">
                         <div className="service-icon">
-                            <i className="fa fa-4x fa-tags" />
-                            <p>本棚</p>
+                            <i className="fas fa-4x fa-pencil-alt" />
+                            <p>レビュー</p>
                         </div>
-                        <p>持っている本を本棚で一括管理</p>
+                        <p className="service-text">読んだ本をレビュー</p>
+                    </div>
+
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <i className="fas fa-4x fa-square" />
+                            <p>Boks</p>
+                        </div>
+                        <p className="service-text">本の感想をまとめる</p>
+                    </div>
+
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <i className="fas fa-4x fa-anchor" />
+                            <p>読書管理</p>
+                        </div>
+                        <p className="service-text">本の読書状況を管理</p>
+                    </div>
+
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <i className="fas fa-4x fa-user-friends" />
+                            <p>フォロー</p>
+                        </div>
+                        <p className="service-text">気になる読書家をフォロー</p>
+                    </div>
+
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <i className="fas fa-4x fa-pencil-alt" />
+                            <p>Bokフロー</p>
+                        </div>
+                        <p className="service-text">自分や他人の最近のBokを確認</p>
                     </div>
 
                     <div className="clear-float-left" />
                 </div>
+            </div>
+        </div>
+
+        <div className="register-wrapper">
+            <div className="container">
+                <Link to="/register" className="btn btn-success">BookBokを始める</Link>
             </div>
         </div>
 
