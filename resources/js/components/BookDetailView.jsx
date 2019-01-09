3da93290107915642d6f190467aad46a1b5cd3eb
Kazuki <kd1210584@st.kobedenshi.ac.jp>
2019-01-09 04:37:30 +0000

    Add BookShelf Button

--- resources/js/components/BookDetailView.jsx
+++ resources/js/components/BookDetailView.jsx
@@ -1,20 +1,50 @@
 import React, { Component } from "react";
-import { fetchBookDetail } from "../actions.js";
+import { fetchBookDetail, storeISBNToUserBookDirect } from "../actions.js";
 import { store } from "../store";
-import { isEmpty } from "../utils.js";
-
+import { getAuthUser, isEmpty } from "../utils";
 import { Loading } from "./shared/Loading";
 import { BookInfo } from "./shared/book/BookInfo";
 
 export class BookDetailView extends Component {
     constructor(props){
         super(props);
-    };
+
+        this.state={ isbn: null, isInvalid: false, invalidMessage: ""};
+        this.handleRegister = this.handleRegister.bind(this);
+    }
 
     componentDidMount(){
         const bookId = parseInt(this.props.match.params.id);
         store.dispatch(fetchBookDetail(bookId));
-    };
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({isbn: nextProps.bookDetail.isbn});
+    }
+
+    handleRegister(e){
+        e.preventDefault();
+
+        const user = getAuthUser();
+        if(isEmpty(user)){
+            return this.props.history.push('/login');
+        }
+
+        storeISBNToUserBookDirect(user.id, this.state.isbn).then(res => {
+            if(res.status === 401){
+                this.setState({ isInvalid: true, invalidMessage: 'ログインが必要です' });
+                throw new Error();
+            }else if(!res.ok){
+                res.json().then(json => {
+                    this.setState({ isInvalid: true, invalidMessage: json.userMessage });
+                });
+                throw new Error();
+            }
+            return res.json();
+        }).then(res => {
+            this.props.history.push(`/users/${res.user.id}/user_books/${res.id}`);
+        }).catch(()=>{});
+    }
 
     render() {
         const book = this.props.bookDetail;
@@ -25,7 +55,12 @@ export class BookDetailView extends Component {
         return (
             <div className="container mt-4">
                 <div className="row justify-content-center">
-                    <div className="col-md-8 main-content p-5">
+                    <div className="col-md-8 main-content p-5 clearfix">
+                        <div className="float-right">
+                            <form onSubmit={this.handleRegister}>
+                                <button type="submit" className="btn btn-success">本棚に追加</button>
+                            </form>
+                        </div>
                         <BookInfo book={book} />
                     </div>
                 </div>
