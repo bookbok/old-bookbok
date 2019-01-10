ab12a4b35218980a8a6bb4ac2b5545510121ca36
AKARI-I <i.akari.1122@gmail.com>
2019-01-10 06:34:46 +0000

    Update like,love position

--- resources/js/components/UserDetailBok.jsx
+++ resources/js/components/UserDetailBok.jsx
@@ -93,20 +93,20 @@ export class UserDetailBok extends Component {
 
                             {/* bok-footer */}
                             <div className="d-flex userd-bok-footer mt-2">
-                                <div className="text-muted userd-page-line-updated">
+                                <div className="text-muted userd-page-line-updated mt-auto">
                                     <div className="userd-page">{page}</div>
                                     <div className="userd-line ml-1 mr-3">{line}</div>
                                     <div className="userd-updated">{bok.updated_at}</div>
                                 </div>
-                                <div className="d-flex ml-auto">
+                                <div className="d-flex ml-auto mt-auto">
                                     <div onClick={(e) => this.clickLike(bok.id)}>
-                                        <p className="liked mr-2">
+                                        <p className="liked mr-2 mt-auto mb-auto">
                                             <i className={this.state.likeClass}></i>
                                             {this.state.likeCount}
                                         </p>
                                     </div>
                                     <div onClick={(e) => this.clickLove(bok.id)}>
-                                        <p className="loved">
+                                        <p className="loved mt-auto mb-auto">
                                             <i className={this.state.loveClass}></i>
                                             {this.state.loveCount}
                                         </p>
