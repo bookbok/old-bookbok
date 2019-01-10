0991d318256e8b17b19db271b2c7f14866bcdab7
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-10 04:48:12 +0000

    Update deploy seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
         App\User::create([
             'name' => 'admin',
             'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
+            'password' => '$2y$10$nYDttVj18d/LVbcug0P70Ot8bvNTdBloQHRhGLdngo7z3jD8hL72u',
             'role_id' => 1,
             'avatar' => 'https://avatars0.githubusercontent.com/u/22770924',
             'created_at' => Carbon::now()->subDays(32),
@@ -26,13 +26,11 @@ class UsersTableSeeder extends Seeder
         App\User::create([
             'name' => 'test-staff',
             'email' => 'staff@example.com',
-            'password' => bcrypt('password'),
+            'password' => '$2y$10$nYDttVj18d/LVbcug0P70Ot8bvNTdBloQHRhGLdngo7z3jD8hL72u',
             'role_id' => 5,
             'avatar' => 'https://avatars0.githubusercontent.com/u/22770924',
             'created_at' => Carbon::now()->subDays(32),
             'updated_at' => Carbon::now()->subDays(32),
         ]);
-
-        factory(App\User::class, 10)->create();
     }
 }
