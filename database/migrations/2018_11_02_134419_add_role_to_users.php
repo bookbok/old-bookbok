7a711dadcfb9f1c793b5b18936170ee7e404cc03
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-11 08:23:07 +0000

    Fix down migrate

--- database/migrations/2018_11_02_134419_add_role_to_users.php
+++ database/migrations/2018_11_02_134419_add_role_to_users.php
@@ -28,6 +28,7 @@ class AddRoleToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
             $table->dropColumn('role_id');
         });
     }
