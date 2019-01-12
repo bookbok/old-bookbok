b6bbfe63db325b368bdac6b30253bdab6a68b611
AKARI-I <i.akari.1122@gmail.com>
2019-01-12 07:18:47 +0000

    Update footer responsive design

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -4,17 +4,21 @@ import { Link } from "react-router-dom";
 export const Footer = () => (
     <div className="container mt-5">
         <div className="justify-content-center">
-            <footer className="d-block">
+            <footer className="d-inline">
                 <nav>
                     <ul className="d-flex">
-                        <li><Link to="/terms_of_service">利用規約</Link></li>
-                        <li><Link to="/privacy">プライバシー</Link></li>
-                        <li><Link to="#">ヘルプ</Link></li>
-                        <li className="mr-auto"><Link to="#">お問い合わせ</Link></li>
-                        <li><Link to="#">BookBokとは</Link></li>
-                        <li><Link to="/users">ユーザー</Link></li>
-                        <li><Link to="#">開発ブログ</Link></li>
-                        <li><Link to="#">ご意見</Link></li>
+                        <div className="d-md-flex">
+                            <li><Link to="/terms_of_service">利用規約</Link></li>
+                            <li><Link to="/privacy">プライバシー</Link></li>
+                            <li><Link to="#">ヘルプ</Link></li>
+                            <li className="mr-auto"><Link to="#">お問い合わせ</Link></li>
+                        </div>
+                        <div className="d-md-flex ml-auto pr-5">
+                            <li><Link to="#">BookBokとは</Link></li>
+                            <li><Link to="/users">ユーザー</Link></li>
+                            <li><Link to="#">開発ブログ</Link></li>
+                            <li><Link to="#">ご意見</Link></li>
+                        </div>
                     </ul>
                 </nav>
             </footer>
