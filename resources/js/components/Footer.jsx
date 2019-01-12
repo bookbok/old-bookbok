2aaa858a74283a40e7e78854b2d146f556a5ca01
AKARI-I <i.akari.1122@gmail.com>
2019-01-12 06:29:53 +0000

    Update footer for smart phone

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => (
     <div className="container mt-5">
         <div className="justify-content-center">
-            <footer className="d-none d-md-block">
+            <footer className="d-block">
                 <nav>
                     <ul className="d-flex">
                         <li><Link to="/terms_of_service">利用規約</Link></li>
