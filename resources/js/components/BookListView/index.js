03525190636c1010bcadfbcc6bedaa0d6554cbee
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-16 07:46:32 +0000

    Refactor suspense component

--- resources/js/components/BookListView/index.js
+++ resources/js/components/BookListView/index.js
@@ -8,32 +8,18 @@ import { isEmpty } from "../../utils";
 import { ConnectedGenres } from "../../containers";
 import { Search } from "../Search";
 import { Loading } from "../shared/Loading";
-import { BookView } from "../BookView";
 import { ISBNModal } from "../shared/book/ISBNModal";
+import BooksSuspense from "./BooksSuspense";
 
-let flg = false;
-const BooksSuspense = (props) => {
-    if (flg == false && !props.books) {
-        throw fetchBookList();
-        flg = true;
-    }
-
-    return books.map((book) => {
-        return <BookView book={book} link={`/books/${book.id}`} key={book.id} />
-    });
-}
 
 class BookListView extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { q: "", isLoading: true };
+        this.state = { q: "" };
         this.handleSubmitSearchText = this.handleSubmitSearchText.bind(this);
         this.handleClickSearchGenre = this.handleClickSearchGenre.bind(this);
     }
-
-    componentDidMount() {
-    };
 
     handleSubmitSearchText(q) {
         this.setState({ q: q }); // 現状stateに入れる必要はないが、ジャンルでの絞り込みも始めた時に必要になりそうなので
@@ -60,6 +46,7 @@ class BookListView extends Component {
                         <ISBNModal />
                     </div>
                     <div className="mt-4 book-list-wrapper">
+                        {/* async component */}
                         <Suspense maxDuration={1000} fallback={<Loading />}>
                             <BooksSuspense books={this.props.books}/>
                             <div className="clear-float-left" />
