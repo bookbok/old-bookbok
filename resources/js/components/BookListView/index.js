a6123d0ea63f443e885ecf5e5606119dfed5e17d
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-16 05:14:47 +0000

    Rename

--- resources/js/components/BookListView/index.js
+++ resources/js/components/BookListView/index.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { fetchBookList } from "../actions";
-import { store } from "../store";
-import { isEmpty } from "../utils";
+import { fetchBookList } from "../../actions";
+import { store } from "../../store";
+import { isEmpty } from "../../utils";
 
-import { ConnectedGenres } from "../containers";
-import { Search } from "./Search";
-import { Loading } from "./shared/Loading";
-import { BookView } from "./BookView";
-import { ISBNModal } from "./shared/book/ISBNModal";
+import { ConnectedGenres } from "../../containers";
+import { Search } from "../Search";
+import { Loading } from "../shared/Loading";
+import { BookView } from "../BookView";
+import { ISBNModal } from "../shared/book/ISBNModal";
+
+let flg = false;
+const BooksSuspense = (props) => {
+    if (flg == false && !props.books) {
+        throw fetchBookList();
+        flg = true;
+    }
+
+    return books.map((book) => {
+        return <BookView book={book} link={`/books/${book.id}`} key={book.id} />
+    });
+}
 
 class BookListView extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { q: "" };
+        this.state = { q: "", isLoading: true };
         this.handleSubmitSearchText = this.handleSubmitSearchText.bind(this);
         this.handleClickSearchGenre = this.handleClickSearchGenre.bind(this);
     }
 
     componentDidMount() {
-        store.dispatch(fetchBookList());
     };
 
     handleSubmitSearchText(q) {
         this.setState({ q: q }); // 現状stateに入れる必要はないが、ジャンルでの絞り込みも始めた時に必要になりそうなので
-        store.dispatch(fetchBookList({ q: q }));
+        fetchBookList({ q: q });
     }
 
     handleClickSearchGenre(genre) {
-        store.dispatch(fetchBookList({ genres: [genre] }));
+        fetchBookList({ genres: [genre] });
     }
 
     render() {
-        if(isEmpty(this.props.books)) {
-            return <Loading />;
-        }
-
-        const books = this.props.books.map((book) => {
-            return <BookView book={book} link={`/books/${book.id}`} key={book.id} />
-        });
-
         return(
             <div className="container mt-4">
                 <div className="row justify-content-center">
@@ -57,8 +60,10 @@ class BookListView extends Component {
                         <ISBNModal />
                     </div>
                     <div className="mt-4 book-list-wrapper">
-                        {books}
-                        <div className="clear-float-left" />
+                        <Suspense maxDuration={1000} fallback={<Loading />}>
+                            <BooksSuspense books={this.props.books}/>
+                            <div className="clear-float-left" />
+                        </Suspense>
                     </div>
                 </div>
             </div>
