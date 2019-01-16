64dac12e401cf35535430ac045cb46c5458cab2e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-16 09:14:36 +0000

    Add the info when empty books

--- resources/js/components/BookListView/BooksSuspense.jsx
+++ resources/js/components/BookListView/BooksSuspense.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchBookList } from "../../actions";
+import { isEmpty } from "../../utils";
 import { BookView } from "../BookView";
 
 const BooksSuspense = (props) => {
@@ -8,6 +9,9 @@ const BooksSuspense = (props) => {
         throw fetchBookList();
     }
 
+    if(isEmpty(props.books)) {
+        return <p className="h5">本が見つかりませんでした</p>;
+    }
     return props.books.map((book) => {
         return <BookView book={book} link={`/books/${book.id}`} key={book.id} />
     });
