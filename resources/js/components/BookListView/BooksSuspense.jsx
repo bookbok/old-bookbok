e81afd8b6da12de3b889d8a9c499e8e9a204f80e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-17 04:52:52 +0000

    Update loader

--- resources/js/components/BookListView/BooksSuspense.jsx
+++ resources/js/components/BookListView/BooksSuspense.jsx
@@ -1,20 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { fetchBookList } from "../../actions";
+import InfiniteScroll from 'react-infinite-scroller';
+import { fetchBookList, fetchMoreBooks } from "../../actions";
 import { isEmpty } from "../../utils";
 import { BookView } from "../BookView";
 
-const BooksSuspense = (props) => {
-    if (!props.books) {
-        throw fetchBookList();
+class BooksSuspense extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasMore: true }
+        this.loadMore = this.loadMore.bind(this);
     }
 
-    if(isEmpty(props.books.data)) {
-        return <p className="h5">本が見つかりませんでした</p>;
+    loadMore(page) {
+        console.log('more');
+        fetchMoreBooks({ page: page }).then(json => {
+            console.log('loaded more')
+            console.log(json)
+            this.setHasMoreBooks(json.next_page_url);
+        });
     }
-    return props.books.data.map((book) => {
-        return <BookView book={book} link={`/books/${book.id}`} key={book.id} />
-    });
+
+    setHasMoreBooks(nextPageURL) {
+        if(nextPageURL) {
+            return this.setState({ hasMore: true });
+        }
+        this.setState({ hasMore: false });
+    }
+
+    render() {
+        if (!this.props.books) {
+            throw fetchBookList().then((json) => {
+                console.log(json)
+                this.setHasMoreBooks(json.next_page_url);
+            });
+        }
+
+        if(isEmpty(this.props.books.data)) {
+            return <p className="h5">本が見つかりませんでした</p>;
+        }
+
+        const books = this.props.books.data.map((book) => {
+            return <BookView book={book} link={`/books/${book.id}`} key={book.id} />
+        });
+        return (
+            <InfiniteScroll
+                pageStart={1}
+                loadMore={this.loadMore}
+                hasMore={this.state.hasMore}>
+                {books}
+            </InfiniteScroll>
+        );
+    }
 }
 
 BooksSuspense.propTypes = {
