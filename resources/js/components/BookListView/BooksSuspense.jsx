641d49522ed3324adc461ee9f948f81870e6a9ec
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-17 14:57:17 +0000

    Fix loading

--- resources/js/components/BookListView/BooksSuspense.jsx
+++ resources/js/components/BookListView/BooksSuspense.jsx
@@ -3,56 +3,39 @@ import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroller';
 import { fetchBookList, fetchMoreBooks } from "../../actions";
 import { isEmpty } from "../../utils";
+import { Loading } from "../shared/Loading";
 import { BookView } from "../BookView";
 
 class BooksSuspense extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { hasMore: false }
+        this.state = { hasMore: true }
         this.loadMore = this.loadMore.bind(this);
     }
 
     loadMore(page) {
-        console.log('more');
+        // HACK: InfiniteScrollのhasMoreプロパティがfalseでも、一度だけ呼び出されてしまうため(ライブラリバグ？)
+        if(this.state.hasMore === false) return;
         fetchMoreBooks({ page: page }).then(json => {
-            console.log('loaded more')
-            console.log(json)
             this.setHasMoreBooks(json.next_page_url);
         });
     }
 
     setHasMoreBooks(nextPageURL) {
-        console.log(nextPageURL)
-        if(nextPageURL) {
-            console.log('hasMore to true');
-            return this.setState({ hasMore: true });
+        if(!nextPageURL) {
+            this.setState({ hasMore: false });
+        } else {
+            this.setState({ hasMore: true });
         }
-        this.setState({ hasMore: false });
-    }
-
-    componentWillReceiveProps(){
-        console.log('props change')
-    }
-    shouldComponentUpdate() {
-        console.log('state or props change')
-        return true;
-    }
-    componentWillUpdate(nextProps, nextState) {
-        console.log('next')
-        console.log(nextState, nextProps)
     }
 
     render() {
         if (!this.props.books) {
-            console.log('more');
             throw fetchBookList().then((json) => {
-                console.log('loaded more')
-                console.log(json)
                 this.setHasMoreBooks(json.next_page_url);
             });
         }
 
-            console.log('render');
         if(isEmpty(this.props.books.data)) {
             return <p className="h5">本が見つかりませんでした</p>;
         }
@@ -65,7 +48,8 @@ class BooksSuspense extends React.Component {
                 initialLoad={false}
                 pageStart={1}
                 loadMore={this.loadMore}
-                hasMore={this.state.hasMore}>
+                hasMore={this.state.hasMore}
+                loader={<Loading key="0"/>}>
                 {books}
             </InfiniteScroll>
         );
