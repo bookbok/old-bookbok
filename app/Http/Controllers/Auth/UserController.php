3936b5aec6ba8cf0d0f61809860edc5d9eefaae8
kento-oka <kento-oka@kentoka.com>
2019-01-17 11:39:50 +0900

    認証ユーザ情報のコントローラーを作成(取得,更新)

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * ユーザ情報
+     *
+     * @param   Request  $request
+     *  リクエスト
+     * 
+     * @return  User
+     */
+    public function show(Request $request)
+    {
+        return $request->user()->makeVisible([
+            'email',
+            'email_verified_at',
+        ]);
+    }
+
+    /**
+     * ユーザー情報更新
+     * 
+     * @param   Request $request
+     *  リクエスト
+     * 
+     * @return  User
+     */
+    public function update(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'name'        => 'required|string|regex:/\A[^[:cntrl:]\s]+\z/u|min:1|max:32|unique:users',
+            'description' => 'required|nullable|string|max:1000',
+            'avatar'      => 'required|string|active_url|max:255',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'userMessage' => $validator->errors()
+            ], 400);
+        }
+
+        $user = $request->user();
+
+        $user->name        = $request->name;
+        $user->description = $request->description;
+        $user->avatar      = $request->avatar;
+
+        $user->save();
+
+        return $user->makeVisible([
+            'email',
+            'email_verified_at',
+        ]);
+    }
+}
