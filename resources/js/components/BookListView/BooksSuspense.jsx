33436feabbf8cefc3e4939373eead71396c57b4f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-17 12:45:09 +0000

    [WIP] Add debug comment

--- resources/js/components/BookListView/BooksSuspense.jsx
+++ resources/js/components/BookListView/BooksSuspense.jsx
@@ -8,7 +8,7 @@ import { BookView } from "../BookView";
 class BooksSuspense extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { hasMore: true }
+        this.state = { hasMore: false }
         this.loadMore = this.loadMore.bind(this);
     }
 
@@ -22,20 +22,37 @@ class BooksSuspense extends React.Component {
     }
 
     setHasMoreBooks(nextPageURL) {
+        console.log(nextPageURL)
         if(nextPageURL) {
+            console.log('hasMore to true');
             return this.setState({ hasMore: true });
         }
         this.setState({ hasMore: false });
     }
 
+    componentWillReceiveProps(){
+        console.log('props change')
+    }
+    shouldComponentUpdate() {
+        console.log('state or props change')
+        return true;
+    }
+    componentWillUpdate(nextProps, nextState) {
+        console.log('next')
+        console.log(nextState, nextProps)
+    }
+
     render() {
         if (!this.props.books) {
+            console.log('more');
             throw fetchBookList().then((json) => {
+                console.log('loaded more')
                 console.log(json)
                 this.setHasMoreBooks(json.next_page_url);
             });
         }
 
+            console.log('render');
         if(isEmpty(this.props.books.data)) {
             return <p className="h5">本が見つかりませんでした</p>;
         }
@@ -45,6 +62,7 @@ class BooksSuspense extends React.Component {
         });
         return (
             <InfiniteScroll
+                initialLoad={false}
                 pageStart={1}
                 loadMore={this.loadMore}
                 hasMore={this.state.hasMore}>
