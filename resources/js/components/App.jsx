66465bdff112f89bd80a43158bf8da1f47905c60
AKARI-I <i.akari.1122@gmail.com>
2019-01-21 02:11:15 +0000

    Add alert switch

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -2,9 +2,37 @@ import React, { Component } from 'react';
 import RouterWithHeader from './RouterWithHeader';
 
 export class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.alertView = this.alertView.bind(this);
+    }
+
+    alertView(type, message){
+        let alertClass = "alert";
+
+        switch(type){
+            case "primary"      : alertClass += " alert-primary";   break;  // skyblue
+            case "secondary"    : alertClass += " alert-secondary"; break;  // gray
+            case "success"      : alertClass += " alert-success";   break;  // green
+            case "info"         : alertClass += " alert-info";      break;  // blue-green
+            case "warning"      : alertClass += " alert-warning";   break;  // yellow
+            case "danger"       : alertClass += " alert-danger";    break;  // red
+            case "light"        : alertClass += " alert-light";     break;  // white
+            case "dark"         : alertClass += " alert-dark";      break;  // dark-gray
+        }
+
+        return <div className={alertClass}>{message}</div>
+    }
+
     render() {
+        const alertMessage = "Alert test";
+
         return (
-            <RouterWithHeader />
+            <div>
+                {this.alertView("success" , "alert test : success")}
+                <RouterWithHeader />
+            </div>
         );
     }
 }
