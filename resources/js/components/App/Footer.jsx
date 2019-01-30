3942435dfa5e911afbdf48bd4feeb756d37a081b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-01-30 07:19:20 +0000

    Update footer view

--- resources/js/components/App/Footer.jsx
+++ resources/js/components/App/Footer.jsx
@@ -2,28 +2,17 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Footer = () => (
-    <div className="container mt-5">
-        <div className="justify-content-center">
-            <footer className="d-inline">
-                <nav>
-                    <ul className="d-flex">
-                        <div className="d-md-flex">
-                            <li><Link to="/terms_of_service">利用規約</Link></li>
-                            <li><Link to="/privacy">プライバシー</Link></li>
-                            <li><Link to="#"><del>ヘルプ</del></Link></li>
-                            <li><Link to="#"><del>お問い合わせ</del></Link></li>
-                        </div>
-                        <div className="d-md-flex ml-auto pr-5">
-                            <li><Link to="#"><del>BookBokとは</del></Link></li>
-                            <li><Link to="/users">ユーザー</Link></li>
-                            <li><Link to="#"><del>開発ブログ</del></Link></li>
-                            <li><Link to="#"><del>ご意見</del></Link></li>
-                        </div>
-                    </ul>
-                </nav>
-            </footer>
-        </div>
-    </div>
+    <footer className="container mt-5">
+        <nav>
+            <ul className="footer-list justify-content-center text-center">
+            {/* <li className="footer-list-item"><Link to="#"><del>BookBokとは</del></Link></li> */}
+                <li className="footer-list-item"><Link to="/terms_of_service">利用規約</Link></li>
+                <li className="footer-list-item"><Link to="/privacy">プライバシー</Link></li>
+                <li className="footer-list-item"><Link to="/users">ユーザー</Link></li>
+                <li className="footer-list-item"><a href="https://goo.gl/forms/Ig6K1ieesR4dY7cg1" target="_blank">ご意見</a></li>
+            </ul>
+        </nav>
+    </footer>
 );
 
 export default Footer;
