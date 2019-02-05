b687ae13ecfd806b5376e5d9ec07e34f4e39ade8
sugino-kazuma <vagrant@vagrant.vm>
2019-02-05 01:00:10 +0000

    UserBookを削除するAPIの実装#471

--- app/Http/Controllers/UserBookController.php
+++ app/Http/Controllers/UserBookController.php
@@ -276,4 +276,34 @@ class UserBookController extends Controller
         return response()->json($userBook);
     }
 
+    /**
+     * 削除API
+     * 
+     * @UserBook $userBook
+     */
+    public function delete(UserBook $userBook){
+        // 存在しないUserBookを指定した場合はサーバが自動的に404を返す
+
+        // 認可チェック
+        $authId = auth()->guard('api')->id();
+        if($authId != $userBook->user_id){
+            return response()->json(
+                [
+                    'status' => 403,
+                    'userMessage' => '自分以外の本を削除することはできません。'
+                ],
+                403
+            );
+        }
+
+        $userBook->delete();
+
+        return response()->json(
+            [
+                'status' => 200,
+                'userMessage' => '削除しました。'
+            ],
+            200
+        );
+    }
 }
