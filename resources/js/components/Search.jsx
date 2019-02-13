71cae44bc725e7fd95314c964e036fbfd57a0a16
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-02-13 07:57:05 +0000

    Update search box text

--- resources/js/components/Search.jsx
+++ resources/js/components/Search.jsx
@@ -12,7 +12,7 @@ export class Search extends Component {
             <div className="input-group form-inline">
                 <input type="search"
                     className="form-control"
-                    placeholder="テキスト入力欄"
+                    placeholder="タイトル / 著者"
                     onChange={e => this.setState({ q: e.target.value })} />
                 <span className="input-group-btn">
                     <button type="button" className="btn btn-light border" onClick={() => this.props.handleSubmit(this.state.q)}>
