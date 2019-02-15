1423cd055847e5eabc478cfefe4b03bccb5fb534
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-02-15 14:41:59 +0000

    Update tutorial component struct

--- resources/js/components/Tutorial/index.js
+++ resources/js/components/Tutorial/index.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class TutorialFrame extends React.Component {
+import Tutorial00 from './Tutorial00';
+import Tutorial01 from './Tutorial01';
+
+
+class Tutorial extends React.Component {
     constructor(props) {
         super(props);
-        this.tutorialTexts = [
-            'チュートリアル開始',
-            '本の登録',
-            'Bokを投稿',
-            'レビューを投稿',
-            'ユーザー情報の編集',
+        this.tutorials = [
+            { component: Tutorial00, title: 'チュートリアルを始めましょう' },
+            { component: Tutorial01, title: '本を登録しましょう' },
+            { component: null, title: 'Bokを投稿しましょう' },
+            { component: null, title: 'レビューを投稿しましょう' },
+            { component: null, title: 'ユーザー情報を編集しましょう' },
         ];
     }
 
     render() {
+        const tutorialID = parseInt(this.props.match.params.id);
+
+        // 基本構造であるページメニューやメインコンテツの入れ物を作成する
+        // 子要素にチュートリアルの種類ごとコンテンツを渡す形
         return (
             <div className="page-content-wrap ">
                 <div className="list-group sub-content">
-                    {this.tutorialTexts.map((text, i) => (
+                    {this.tutorials.map((tutorial, i) => (
                         <Link to={`/tutorial/${i}`}
-                            className={`list-group-item list-group-item-action ${this.props.tutorialID == i && 'active'}`}
+                            className={`list-group-item list-group-item-action ${tutorialID === i && 'active'}`}
                             key={i}>
-                            {text}
+                            {tutorial.title}
                         </Link>
                     ))}
                 </div>
@@ -29,39 +37,19 @@ class TutorialFrame extends React.Component {
                 <div className="container mt-4">
                     <div className="row justify-content-center">
                         <div className="col-md-8 main-content p-5">
-                            {this.props.children}
+                            {(() => {
+                                const TutorialContent = this.tutorials[tutorialID];
+                                if(!TutorialContent || !TutorialContent.component) return null;
+
+                                return <TutorialContent.component
+                                    title={this.tutorials[tutorialID].title} />;
+                            })()}
                         </div>
                     </div>
                 </div>
+
             </div>
         );
-    }
-}
-
-class Tutorial00 extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>チュートリアル開始</h1>
-                <p>読書をもっと素敵にする「BookBok」サービスをご利用いただきありがとうございます。</p>
-            </div>
-        );
-    }
-}
-
-class Tutorial extends React.Component {
-    render() {
-        const tutorialID = this.props.match.params.id;
-
-        // 基本構造であるページメニューやメインコンテツの入れ物を作成する
-        // 子要素にチュートリアルの種類ごとコンテンツを渡す形
-        const page = <TutorialFrame tutorialID={tutorialID}>
-            {(() => {
-                if(tutorialID == 0) return <Tutorial00 />;
-            })()}
-        </TutorialFrame>
-
-        return page;
     }
 }
 
