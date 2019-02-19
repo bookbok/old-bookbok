75aefea982f3c344df7a5bdc2f8fe7efaeeb590c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-02-19 14:23:47 +0000

    Refactor

--- resources/js/components/Tutorial/Tutorial02.jsx
+++ resources/js/components/Tutorial/Tutorial02.jsx
@@ -6,7 +6,9 @@ class Tutorial02 extends React.Component {
         return (
             <div>
                 <h1>{this.props.title}</h1>
-                <p>BookBokではBokという本を読んでいる間の呟きを投稿する仕組みがあります。</p>
+                <p>
+                    BookBokではBokという、本を読んでいる間の呟きを投稿する仕組みがあります。
+                </p>
 
                 <div className="text-center">
                     <Link to="/tutorial/3" className="btn btn-success">次へ進む</Link>
