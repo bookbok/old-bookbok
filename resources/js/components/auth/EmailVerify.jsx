562ca8a05fbfb462dda8ea6447bcae589086e45b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-02-21 18:47:09 +0000

    Add email input

--- resources/js/components/auth/EmailVerify.jsx
+++ resources/js/components/auth/EmailVerify.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { store } from "../../store";
-import { successfulStatus } from '../../utils';
+import { successfulStatus, getAuthUser } from '../../utils';
 
 import { Loading } from '../shared/Loading';
 import { verifyEmail, resendVerifyMail, setAlertMessage } from '../../actions';
@@ -11,15 +11,16 @@ class EmailVerify extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { verified: false };
+        this.state = { verified: false, email: '', show: false };
         this.handleClickResend = this.handleClickResend.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
         verifyEmail(location.href).then(json => {
             if(successfulStatus(json.status) || !json.status) {
                 store.dispatch(setAlertMessage("success", {__html: json.userMessage}));
-                this.props.history.push('/login');
+                //this.props.history.push('/login');
             } else {
                 store.dispatch(setAlertMessage("warning", {__html: json.userMessage}));
             }
@@ -27,8 +28,19 @@ class EmailVerify extends React.Component {
         });
     }
 
-    handleClickResend() {
-        resendVerifyMail().then(json => {
+    handleClickResend(email) {
+        let destinationEmail = null;
+        const currentUser = getAuthUser();
+        if(currentUser) {
+            destinationEmail = currentUser.email;
+        } else if(email) {
+            destinationEmail = email;
+        } else {
+            this.setState({ show: true });
+            return;
+        }
+
+        resendVerifyMail(destinationEmail).then(json => {
             // HACK: 200系で帰るときだけstatusが設定されていない場合が多々あるためこのような記法になっている
             if(successfulStatus(json.status) || !json.status) {
                 store.dispatch(setAlertMessage("success", {__html: json.userMessage}));
@@ -36,6 +48,11 @@ class EmailVerify extends React.Component {
                 store.dispatch(setAlertMessage("warning", {__html: json.userMessage}));
             }
         });
+    }
+
+    handleChange(e) {
+        const name = e.target.name;
+        this.setState({ [name]: e.target.value });
     }
 
     render() {
@@ -53,7 +70,34 @@ class EmailVerify extends React.Component {
 
                             <div className="card-body">
                                 もし検証メールを確認していない場合、メールが届いていないかご確認ください。<br/>
-                                もう一度メールを送信する場合は<Link to="#" onClick={this.handleClickResend}>こちらをクリックしてください。</Link>
+                                もう一度メールを送信する場合は
+
+                                <div className="dropdown">
+                                    <a role="button"
+                                        className="text-primary"
+                                        aria-haspopup="true"
+                                        aria-expanded="false"
+                                        onClick={() => this.handleClickResend()} >
+                                        こちらをクリックしてください。
+                                    </a>
+
+                                    <div className={`dropdown-menu p-4 ${this.state.show ? 'show': ''}`}>
+                                        <div className="form-group">
+                                            <label htmlFor="email">送信先email</label>
+                                            <input name="email"
+                                                type="email"
+                                                className="form-control"
+                                                placeholder="email@example.com"
+                                                value={this.state.email}
+                                                onChange={this.handleChange} />
+                                        </div>
+                                        <button className="btn btn-success"
+                                            onClick={() => this.handleClickResend(this.state.email)}>
+                                            送信
+                                        </button>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>{/* end card */}
 
