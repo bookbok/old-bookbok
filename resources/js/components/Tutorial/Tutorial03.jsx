6bd387e3e244bf421d236f96d92c04b7e20f1918
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-02-21 11:37:13 +0000

    Add to tutorial ver 4

--- resources/js/components/Tutorial/Tutorial03.jsx
+++ resources/js/components/Tutorial/Tutorial03.jsx
@@ -16,6 +16,27 @@ class Tutorial03 extends React.Component {
                 <hr/>
 
                 <p>
+                    レビューを書くにはまず自分の本棚に移動する必要があります。
+                    画面右上のアカウント名をクリックし、表示される一覧の「プロフィール」からユーザーページに遷移できます。
+                    <ImageCard src="/images/tutorials/03-01.png" />
+                </p>
+
+                <p>
+                    その後、タブの本棚を選択すると自分の本棚が表示されます。
+                    <ImageCard src="/images/tutorials/03-02.png" />
+                </p>
+
+                <p>
+                    本棚に移動した後、自分の本棚からどれか本の画像を選択します。
+                    遷移した画面を下にスクロールし「レビュー」というボタンを選択します。
+                    <ImageCard src="/images/tutorials/03-02.png" />
+                </p>
+
+                <p>
+                    レビューの入力フォームが表示されます。
+                    タイトル、内容を入力できる様になっています。
+                    タイトルは任意入力で、入力しなかった場合「無題」と表示されます。
+                    <ImageCard src="/images/tutorials/03-02.png" />
                 </p>
 
                 <div className="text-center">
