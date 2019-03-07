2bf0f5f221cba55c1c1a31d6bd69a253231bd10a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-07 14:44:36 +0000

    Add a tutorial link to footer

--- resources/js/components/App/Footer.jsx
+++ resources/js/components/App/Footer.jsx
@@ -8,6 +8,7 @@ const Footer = () => (
             {/* <li className="footer-list-item"><Link to="#"><del>BookBokとは</del></Link></li> */}
                 <li className="footer-list-item"><Link to="/terms_of_service">利用規約</Link></li>
                 <li className="footer-list-item"><Link to="/privacy">プライバシー</Link></li>
+                <li className="footer-list-item"><Link to="/tutorial/0">チュートリアル</Link></li>
                 <li className="footer-list-item"><Link to="/users">ユーザー</Link></li>
                 <li className="footer-list-item"><a href="https://goo.gl/forms/Ig6K1ieesR4dY7cg1" target="_blank">ご意見</a></li>
             </ul>
