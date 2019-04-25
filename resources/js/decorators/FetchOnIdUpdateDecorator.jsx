6623588ec79f163d464aa219387cc096a39c02b5
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-25 06:49:45 +0000

    Add propTypes to decorator

--- resources/js/decorators/FetchOnIdUpdateDecorator.jsx
+++ resources/js/decorators/FetchOnIdUpdateDecorator.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { withRouter } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 // 同じコンポーネントだとパス内のidが変わってもcomponentDidMountが呼ばれない
 // そのためパスのidが変わったら再度API読み込みをする共通処理をDecoratorパターンで実装
 export function fetchOnIdUpdateDecorator(updater) {
-    return (DecoratedComponent) => withRouter(
+    return DecoratedComponent => {
         class FetchOnIdUpdateDecorator extends React.Component {
             componentWillReceiveProps(nextProps) {
                 const params = this.props.match.params;
                 const nextParams = nextProps.match.params;
                 Object.keys(params).some(key => {
-                    if(params[key] !== nextParams[key]) {
+                    if (params[key] !== nextParams[key]) {
                         updater(nextParams);
                         return true;
                     }
@@ -21,7 +22,13 @@ export function fetchOnIdUpdateDecorator(updater) {
                 return <DecoratedComponent {...this.props} />;
             }
         }
-    )
+
+        FetchOnIdUpdateDecorator.propTypes = {
+            match: PropTypes.shape({
+                params: PropTypes.object,
+            }),
+        };
+
+        return withRouter(FetchOnIdUpdateDecorator);
+    };
 }
-
-
