a7a70c5c5e969f8952015fa17bb77ad4a18a53a6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-26 07:36:25 +0000

    Refactor book view with linter

--- resources/js/components/BookView.jsx
+++ resources/js/components/BookView.jsx
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export class BookView extends Component {
     render() {
         const book = this.props.book;
         let bookName;
 
-        if(book.name != "") {
+        if (book.name != '') {
             const width = screen.width <= 750 ? 12 : 14;
-            if(book.name.charAt(width+1) != "") {
-                bookName = <pre className="book-view-pre">{book.name.slice(0, width)}<br/>{book.name.slice(width)}</pre>;
+            if (book.name.charAt(width + 1) != '') {
+                bookName = (
+                    <pre className="book-view-pre">
+                        {book.name.slice(0, width)}
+                        <br />
+                        {book.name.slice(width)}
+                    </pre>
+                );
             } else {
-                bookName = <pre className="book-view-pre">{book.name.slice(0, width)}<br/><br/></pre>;
+                bookName = (
+                    <pre className="book-view-pre">
+                        {book.name.slice(0, width)}
+                        <br />
+                        <br />
+                    </pre>
+                );
             }
         } else {
-                bookName = <pre className="book-view-pre"><br/><br/></pre>;
+            bookName = (
+                <pre className="book-view-pre">
+                    <br />
+                    <br />
+                </pre>
+            );
         }
 
         return (
-            <div className={this.props.className || "book-view-box"}>
-                <Link to={this.props.link ? this.props.link : "#"}>
-                    <img src={book.cover}
-                        className="book-view-img"
-                        alt={book.name} />
+            <div className={this.props.className || 'book-view-box'}>
+                <Link to={this.props.link ? this.props.link : '#'}>
+                    <img src={book.cover} className="book-view-img" alt={book.name} />
                     {bookName}
                 </Link>
             </div>
-         );
-
+        );
     }
 }
