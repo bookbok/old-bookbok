896ea421134ab986bdb02aae94332bd98390f2cc
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-26 05:10:01 +0000

    Refactor with linter

--- resources/js/components/shared/user/FloatUserInfo.jsx
+++ resources/js/components/shared/user/FloatUserInfo.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { store } from '../../../store';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { withRouter } from "react-router-dom";
-import { getAuthUser, isEmpty } from '../../../utils';
-import { requestFollow, requestUnFollow,
-         setAlertMessage } from '../../../actions';
+import { withRouter } from 'react-router-dom';
+import { getAuthUser } from '../../../utils';
+import { requestFollow, requestUnFollow, setAlertMessage } from '../../../actions';
 import FollowButton from './FollowButton';
 import DefaultAvatar from './DefaultAvatar';
 
@@ -31,16 +30,19 @@ export class FloatUserInfo_ extends Component {
 
     handleClickFollow() {
         const user = getAuthUser();
-        if(!user){
-            store.dispatch(setAlertMessage("warning", {__html: "<div><a href='/login'>ログイン</a>してください</div>"}));
+        if (!user) {
+            store.dispatch(
+                setAlertMessage('warning', {
+                    __html: "<div><a href='/login'>ログイン</a>してください</div>",
+                })
+            );
             return;
         }
 
-
-        if(this.state.followed) {
+        if (this.state.followed) {
             requestUnFollow(user.id, this.props.user.id).then(() => {
                 this.setState({ followed: !this.state.followed });
-            })
+            });
         } else {
             requestFollow(user.id, this.props.user.id).then(() => {
                 this.setState({ followed: !this.state.followed });
@@ -56,29 +58,42 @@ export class FloatUserInfo_ extends Component {
                 <div className="d-flex">
                     <div>
                         <Link to={`/users/{user.id}`} className="text-body">
-                            { user.avatar ?
-                                <img src={user.avatar} className="user-info-avatar" /> :
+                            {user.avatar ? (
+                                <img src={user.avatar} className="user-info-avatar" />
+                            ) : (
                                 <DefaultAvatar className="user-info-avatar bg-bookbok" />
-                            }
+                            )}
                             <p className="h4 font-weight-bold">{user.name}</p>
                         </Link>
                     </div>
 
                     <div className="user-follow-info mt-2">
-                        <Link to={`/users/${user.id}/followings`} className="m-2">{user.following_count} フォロー</Link>
-                        <Link to={`/users/${user.id}/followers`} className="m-2">{user.follower_count} フォロワー</Link>
+                        <Link to={`/users/${user.id}/followings`} className="m-2">
+                            {user.following_count} フォロー
+                        </Link>
+                        <Link to={`/users/${user.id}/followers`} className="m-2">
+                            {user.follower_count} フォロワー
+                        </Link>
                     </div>
                 </div>
                 <FollowButton
                     userId={user.id}
                     followed={this.state.followed}
-                    handleClickFollow={this.handleClickFollow} />
+                    handleClickFollow={this.handleClickFollow}
+                />
 
                 <div className="user-info-accordion mt-2">
-                    <label htmlFor="user-info-accordion-check" className="accordion-label text-center mt-2">
-                        <i className="fas fa-angle-down"></i> 詳細を表示する
+                    <label
+                        htmlFor="user-info-accordion-check"
+                        className="accordion-label text-center mt-2"
+                    >
+                        <i className="fas fa-angle-down" /> 詳細を表示する
                     </label>
-                    <input type="checkbox" id="user-info-accordion-check" className="user-info-accordion-check" />
+                    <input
+                        type="checkbox"
+                        id="user-info-accordion-check"
+                        className="user-info-accordion-check"
+                    />
                     <pre className="user-info-description mt-4 mb-0">
                         {user.description ? user.description : '未設定'}
                     </pre>
@@ -98,7 +113,7 @@ FloatUserInfo_.propTypes = {
         following_count: PropTypes.number.isRequired,
         is_follower: PropTypes.bool.isRequired,
         is_following: PropTypes.bool.isRequired,
-    })
+    }),
 };
 
 export const FloatUserInfo = withRouter(FloatUserInfo_);
--- resources/js/components/shared/user/SimpleUser.jsx
+++ resources/js/components/shared/user/SimpleUser.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { withRouter } from "react-router-dom";
+import * as ResourceTypes from 'resource-types';
+import { withRouter } from 'react-router-dom';
 import { requestFollow, requestUnFollow } from '../../../actions';
 import { getAuthUser, isEmpty } from '../../../utils';
 import FollowButton from './FollowButton';
@@ -24,15 +24,15 @@ class SimpleUser_ extends Component {
 
     handleClickFollow() {
         const user = getAuthUser();
-        if(isEmpty(user)) {
+        if (isEmpty(user)) {
             console.log('ログインが必要です');
             return this.props.history.push('/login');
         }
 
-        if(this.state.followed) {
+        if (this.state.followed) {
             requestUnFollow(user.id, this.props.user.id).then(() => {
                 this.setState({ followed: !this.state.followed });
-            })
+            });
         } else {
             requestFollow(user.id, this.props.user.id).then(() => {
                 this.setState({ followed: !this.state.followed });
@@ -43,26 +43,29 @@ class SimpleUser_ extends Component {
     render() {
         const user = this.props.user;
         // ユーザー一覧画面とフォロワー一覧などの両方でフォローボタンの表示を分岐したいため
-        const followBtn = user.is_follower == null || user.is_following == null ? (
-            <div/>
-        ) : (
-            <div className="ml-auto">
-                <FollowButton followed={this.state.followed} handleClickFollow={this.handleClickFollow} />
-            </div>
-        );
+        const followBtn =
+            user.is_follower == null || user.is_following == null ? (
+                <div />
+            ) : (
+                <div className="ml-auto">
+                    <FollowButton
+                        followed={this.state.followed}
+                        handleClickFollow={this.handleClickFollow}
+                    />
+                </div>
+            );
 
         return (
             <div className="card">
                 <div className="card-body d-flex">
-                    { user.avatar ?
-                        <img src={user.avatar} className="user-list-avatar" /> :
+                    {user.avatar ? (
+                        <img src={user.avatar} className="user-list-avatar" />
+                    ) : (
                         <DefaultAvatar className="user-list-avatar bg-bookbok" />
-                    }
+                    )}
                     <div className="ml-2">
                         <Link to={`/users/${user.id}`}>{user.name}</Link>
-                        <div className="small text-muted">
-                            {user.created_at}
-                        </div>
+                        <div className="small text-muted">{user.created_at}</div>
                     </div>
                     {followBtn}
                 </div>
@@ -72,15 +75,8 @@ class SimpleUser_ extends Component {
 }
 
 SimpleUser_.propTypes = {
-    user: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        avatar: PropTypes.string.isRequired,
-        follower_count: PropTypes.number,
-        following_count: PropTypes.number,
-        is_follower: PropTypes.bool,
-        is_following: PropTypes.bool,
-    })
+    history: ResourceTypes.ROUTER,
+    user: ResourceTypes.USER,
 };
 
 const SimpleUser = withRouter(SimpleUser_);
