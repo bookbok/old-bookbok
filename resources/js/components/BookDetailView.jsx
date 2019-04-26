dd765e34b96ca658b5e3148e0d8b86da77a091aa
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-26 06:49:50 +0000

    Refactor with linter

--- resources/js/components/BookDetailView.jsx
+++ resources/js/components/BookDetailView.jsx
@@ -1,52 +1,61 @@
-import React, { Component } from "react";
-import { fetchBookDetail, storeISBNToUserBookDirect, setAlertMessage } from "../actions.js";
-import { store } from "../store";
-import { getAuthUser, isEmpty, toLines } from "../utils";
+import React, { Component } from 'react';
+import { fetchBookDetail, storeISBNToUserBookDirect, setAlertMessage } from '../actions.js';
+import { store } from '../store';
+import { getAuthUser, isEmpty, toLines } from '../utils';
 
 import { Link } from 'react-router-dom';
-import { Loading } from "./shared/Loading";
-import { BookInfo } from "./shared/book/BookInfo";
+import { Loading } from './shared/Loading';
+import { BookInfo } from './shared/book/BookInfo';
 import BackButtonArea from './shared/BackButtonArea';
 
 export class BookDetailView extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.handleRegister = this.handleRegister.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const bookId = parseInt(this.props.match.params.id);
         store.dispatch(fetchBookDetail(bookId));
     }
 
-    handleRegister(e){
+    handleRegister(e) {
         e.preventDefault();
 
         const user = getAuthUser();
-        if(isEmpty(user)){
-            store.dispatch(setAlertMessage("warning", {__html: "<div><a href='/login'>ログイン</a>してください</div>"}));
+        if (isEmpty(user)) {
+            store.dispatch(
+                setAlertMessage('warning', {
+                    __html: "<div><a href='/login'>ログイン</a>してください</div>",
+                })
+            );
             return;
         }
 
-        storeISBNToUserBookDirect(user.id, this.props.bookDetail.isbn).then(res => {
-            if(res.status === 401){
-                throw new Error();
-            }else if(!res.ok){
-                res.json().then(json => {
-                    store.dispatch(setAlertMessage("warning", {__html: `<div>${json.userMessage}</div>`}));
-                });
-                throw new Error();
-            }
-            return res.json();
-        }).then(res => {
-            this.props.history.push(`/users/${res.user.id}/user_books/${res.id}`);
-        }).catch(()=>{});
+        storeISBNToUserBookDirect(user.id, this.props.bookDetail.isbn)
+            .then(res => {
+                if (res.status === 401) {
+                    throw new Error();
+                } else if (!res.ok) {
+                    res.json().then(json => {
+                        store.dispatch(
+                            setAlertMessage('warning', { __html: `<div>${json.userMessage}</div>` })
+                        );
+                    });
+                    throw new Error();
+                }
+                return res.json();
+            })
+            .then(res => {
+                this.props.history.push(`/users/${res.user.id}/user_books/${res.id}`);
+            })
+            .catch(() => {});
     }
 
     render() {
         const book = this.props.bookDetail;
-        if(isEmpty(book)){
+        if (isEmpty(book)) {
             return <Loading />;
         }
 
@@ -54,9 +63,7 @@ export class BookDetailView extends Component {
             <div key={review.id}>
                 <div className="card p-2">
                     <pre className="userd-bok-user border-bottom">
-                        <Link to={`/users/${review.user_id}`}>
-                            {review.name}
-                        </Link>
+                        <Link to={`/users/${review.user_id}`}>{review.name}</Link>
                         &nbsp;さんのレビュー
                     </pre>
                     <pre className="userd-bok-body mt-2 mr-2">{review.body}</pre>
@@ -68,10 +75,12 @@ export class BookDetailView extends Component {
             <div className="container mt-4">
                 <div className="row justify-content-center">
                     <div className="main-content clearfix">
-                        <BackButtonArea to="/books"/>
+                        <BackButtonArea to="/books" />
                         <div className="float-right">
                             <form onSubmit={this.handleRegister}>
-                                <button type="submit" className="btn btn-success">本棚に追加</button>
+                                <button type="submit" className="btn btn-success">
+                                    本棚に追加
+                                </button>
                             </form>
                         </div>
                         <BookInfo book={book} />
