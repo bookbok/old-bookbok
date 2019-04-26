9a0628e77303bf8f785b5766478cfce77a70bc06
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-26 04:46:37 +0000

    Refactor with linter

--- resources/js/components/shared/ErrorsView.jsx
+++ resources/js/components/shared/ErrorsView.jsx
@@ -1,26 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 
 export class ErrorsView extends Component {
     render() {
         const errors = this.props.errors;
-        if(!errors) {
-            return <div/>;
+        if (!errors) {
+            return <div />;
         }
 
         // 文字列、配列、オブジェクトどれでも対応する処理
         let composedErrors;
-        if(typeof errors === 'string'){
+        if (typeof errors === 'string') {
             composedErrors = <li key={0}>{errors}</li>;
         } else {
-            composedErrors = <div/>;
+            composedErrors = <div />;
         }
 
         return (
             <div className="errors-font">
-                <ul className="text-danger">
-                    {composedErrors}
-                </ul>
+                <ul className="text-danger">{composedErrors}</ul>
             </div>
         );
     }
 }
+
+ErrorsView.propTypes = {
+    errors: PropTypes.object.isRequired,
+};
