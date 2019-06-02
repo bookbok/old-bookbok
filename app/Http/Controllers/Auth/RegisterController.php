a3044341f554eaba08b28b12c0f0144045d42520
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-02 13:46:28 +0000

    Update to form request

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,32 +6,20 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Events\Registered;
+use App\Http\Requests\UserRequest;
 
 class RegisterController extends Controller
 {
     /**
      * ユーザ登録処理
      *
-     * @param   Request    $request
+     * @param   UserRequest    $request
      *  リクエスト
      *
      * @return  \Illuminate\Http\Response
      */
-    public function register(Request $request)
+    public function register(UserRequest $request)
     {
-        $validator = \Validator::make($request->all(), [
-            'name'     => 'required|string|regex:/\A[^[:cntrl:]\s]+\z/u|min:1|max:32|unique:users',
-            'email'    => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 400,
-                'userMessage' => $validator->errors()
-            ], 400);
-        }
-
         $data = $request->all();
         $user = User::create([
             'name'     => $data['name'],
