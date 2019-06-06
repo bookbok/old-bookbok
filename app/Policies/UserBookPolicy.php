03ac73026dfd8f4abb1d1d4404fa44931790f8ba
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-06 10:54:32 +0000

    Add user book policy

---
+++ app/Policies/UserBookPolicy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\UserBook;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\AuthorizationException;
+
+class UserBookPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the user book.
+     *
+     * @param  \App\User  $user
+     * @param  \App\UserBook  $userBook
+     * @return mixed
+     */
+    public function view(User $user, UserBook $userBook)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create user books.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the user book.
+     *
+     * @param  \App\User  $user
+     * @param  \App\UserBook  $userBook
+     * @return mixed
+     */
+    public function update(User $user, UserBook $userBook)
+    {
+        if ($user->id == $userBook->user_id) {
+            return true;
+        }
+        throw new AuthorizationException('自分以外の本棚を編集することはできません。');
+    }
+
+    /**
+     * Determine whether the user can delete the user book.
+     *
+     * @param  \App\User  $user
+     * @param  \App\UserBook  $userBook
+     * @return mixed
+     */
+    public function delete(User $user, UserBook $userBook)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the user book.
+     *
+     * @param  \App\User  $user
+     * @param  \App\UserBook  $userBook
+     * @return mixed
+     */
+    public function restore(User $user, UserBook $userBook)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the user book.
+     *
+     * @param  \App\User  $user
+     * @param  \App\UserBook  $userBook
+     * @return mixed
+     */
+    public function forceDelete(User $user, UserBook $userBook)
+    {
+        //
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        'App\UserBook' => 'App\Policies\UserBookPolicy'
     ];
 
     /**
