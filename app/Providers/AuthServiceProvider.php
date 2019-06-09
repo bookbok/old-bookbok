6a8a2cb0f7509334db2541081ec3f7bf4c9b2479
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-09 13:44:06 +0000

    Add follower policy file

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -17,7 +17,8 @@ class AuthServiceProvider extends ServiceProvider
         'App\Model' => 'App\Policies\ModelPolicy',
         'App\UserBook' => 'App\Policies\UserBookPolicy',
         'App\Bok' => 'App\Policies\BokPolicy',
-        'App\Review' => 'App\Policies\ReviewPolicy'
+        'App\Review' => 'App\Policies\ReviewPolicy',
+        'App\Follower' => 'App\Policies\FollowerPolicy'
     ];
 
     /**
