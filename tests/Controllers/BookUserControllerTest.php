46bd15f1f0620a475bfb4825e973571b0d70e578
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-14 10:54:36 +0000

    Update test

--- tests/Controllers/BookUserControllerTest.php
+++ tests/Controllers/BookUserControllerTest.php
@@ -5,31 +5,36 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\UserBook;
-use App\Book;
-use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 
-
-use Laravel\Passport\ClientRepository;
-use Illuminate\Support\Facades\DB;
-use App\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Components\BookInfoScraper\ScrapeManager;
 use App\Http\Controllers\UserBookController;
-use Illuminate\Http\Request;
+use App\User;
+use App\Book;
+use App\UserBook;
 
 class UserBookControllerTest extends TestCase
 {
     use RefreshDatabase;
-    //use DatabaseTransactions;
 
-    protected $headersWithToken = [];
-    protected $headersWithoutToken = [];
-    protected $scopes = [];
     protected $user;
+    protected $book;
 
-    /** @var CuzzleHttp\Client */
-    private $client;
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->book = factory(Book::class)->make();
+
+        $mock = \Mockery::mock(ScrapeManager::class)
+            ->shouldReceive('searchByIsbn')
+            ->andReturn($this->book)
+            ->getMock();
+        $this->app->bind('app.bookInfo.scrapeManager', function() use ($mock) {
+            return $mock;
+        });
+    }
 
     public function tearDown()
     {
@@ -41,56 +46,15 @@ class UserBookControllerTest extends TestCase
      * @test
      */
     public function BOOKに登録されていないISBNを入力されたとき、および登録されているISBNを入力されたときのテスト(){
-        // Personal Access ClientをDBに作成
-        $clientRepository = new ClientRepository();
-        $client = $clientRepository->createPersonalAccessClient(
-            null, 'Test Personal Access Client', url('/')
-        );
-        DB::table('oauth_personal_access_clients')->insert([
-            'client_id' => $client->id,
-            'created_at' => new \DateTime,
-            'updated_at' => new \DateTime,
-        ]);
-
-        // テストユーザの作成
-        $this->user = factory(User::class)->create();
-        $token = $this->user->createToken('TestToken', $this->scopes)->accessToken;
-
-        // トークンありのリクエストのヘッダーを設定: User
-        $this->headersWithToken['Content-Type'] = 'application/json';
-        $this->headersWithToken['Accept'] = 'application/json';
-        $this->headersWithToken['Authorization'] = 'Bearer ' . $token;
-
-        // トークンなしのリクエストのヘッダーを設定: User
-        $this->headersWithoutToken['Content-Type'] = 'application/json';
-        $this->headersWithoutToken['Accept'] = 'application/json';
-
-        $book = factory(Book::class)->make();
-        $mock = \Mockery::mock(ScrapeManager::class)
-            ->shouldReceive('searchByIsbn')
-            ->twice()
-            //->once() // 1度しか呼ばれないことを期待する
-            ->andReturn($book)
-            ->getMock();
-        $this->app->bind('app.bookInfo.scrapeManager', function() use ($mock) {
-            return $mock;
-        });
-
         $this->actingAs($this->user, 'api');
+
         $request = new Request(['isbn' => '9784063842760']);
         $response = \App::make(UserBookController::class)->store($request, $this->user);
-
         $this->assertEquals(201, $response->status());
         $data = collect($response->getData());
+        $this->assertTrue($data->has(['book', 'user', 'review', 'boks']));
 
-        $this->assertTrue($data->contains('user'));
-        $this->assertTrue($data->contains('book'));
-        $this->assertTrue($data->contains('review'));
-        $this->assertTrue($data->contains('boks'));
-
-
-        $this->otherUser = factory(User::class)->create();
-        $response = \App::make(UserBookController::class)->store($request, $this->otherUser);
+        $response = \App::make(UserBookController::class)->store($request, $this->user);
         $this->assertEquals(409, $response->status());
     }
 }
