882326cca19b14b023e448b0fbcc4fed8290669d
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-19 15:08:39 +0000

    Add test of Book@index

---
+++ tests/Controllers/BookControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+
+use App\Components\BookInfoScraper\ScrapeManager;
+use App\Http\Controllers\BookController;
+use App\User;
+use App\Book;
+
+class BookControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+    protected $book;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->book = factory(Book::class)->make();
+
+        $mock = \Mockery::mock(ScrapeManager::class)
+            ->shouldReceive('searchByIsbn')
+            ->andReturn($this->book)
+            ->getMock();
+        $this->app->bind('app.bookInfo.scrapeManager', function() use ($mock) {
+            return $mock;
+        });
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        \Mockery::close();
+    }
+
+    public function test本の一覧を取得する() {
+        factory(Book::class, 2)->create();
+
+        $request = new Request();
+        $response = \App::make(BookController::class)->index($request);
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals(2, count($response->getData()->data));
+    }
+}
