22719c97bcb38a3cbb8b74d6344e918d1e74342f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-28 05:29:18 +0000

    Add GenreController test

---
+++ tests/Controllers/GenreControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\GenreController;
+use App\User;
+use App\Genre;
+
+class GenreControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testジャンルの一覧を取得する() {
+        factory(Genre::class, 2)->create();
+
+        $response = \App::make(GenreController::class)->index();
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals(2, count($response->getData()));
+    }
+
+    public function testジャンルの詳細を取得する() {
+        $genre = factory(Genre::class)->create();
+
+        $response = \App::make(GenreController::class)->show($genre);
+        $this->assertEquals(200, $response->status());
+    }
+}
+
