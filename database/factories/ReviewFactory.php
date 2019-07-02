6d203d95d9be39cd64b9264686eb3d0c39b2919b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-07-02 00:50:08 +0000

    Add auto data create

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,7 +4,16 @@ use Faker\Generator as Faker;
 use Carbon\Carbon;
 
 $factory->define(App\Review::class, function (Faker $faker) {
+    $user = factory(App\User::class)->create();
     return [
+        'user_id' => function () {
+            return $user->id;
+        },
+        'user_book_id' => function () {
+            return factory(App\UserBook::class)->create([
+                'user_id' => $user->id,
+            ])->id;
+        },
         'body' => $faker->realText($maxNbChars = 255, $indexSize = 2),
         'published_at' => $faker->dateTimeBetween($startDate = '-1 month', $endDate = 'now'),
     ];
