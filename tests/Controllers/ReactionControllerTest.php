a9809d1a6bc29cf3aff9b227d4b645f92d0b9c4d
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-07-02 05:00:41 +0000

    Add test of ReactionController@userLikes

---
+++ tests/Controllers/ReactionControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\ReactionController;
+use App\User;
+use App\UserBook;
+use App\Bok;
+use App\Reaction;
+
+class ReactionControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    public function test自分の良いね一覧を取得する() {
+        $this->actingAs($this->user, 'api');
+        $userBook = factory(UserBook::class)->create(['user_id' => $this->user->id]);
+        $bok = factory(Bok::class)->create([
+            'user_id' => $this->user->id,
+            'user_book_id' => $userBook->id,
+        ]);
+        $followers = factory(Reaction::class)->create([
+            'user_id' => $this->user->id,
+            'bok_id' => $bok->id,
+            'liked' => true
+        ]);
+
+        $response = \App::make(ReactionController::class)->userLikes($this->user->id);
+        $this->assertEquals(200, $response->status());
+    }
+
+}
