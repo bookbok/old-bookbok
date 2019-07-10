1c0fc735edb6e529720629c076de9267f3db9e77
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-07-10 01:01:06 +0000

    Update to model scope

--- app/Follower.php
+++ app/Follower.php
@@ -9,4 +9,8 @@ class Follower extends Model
     protected $fillable = [
         'user_id', 'target_id',
     ];
+
+    public function scopeWhereFromForeignKeys($query, $userId, $targetId) {
+        return $query->where('user_id', $userId)->where('target_id', $targetId);
+    }
 }
