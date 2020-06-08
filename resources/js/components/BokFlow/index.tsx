d51e702ca6a188746d7bb1336ff3d56581f9a47e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2020-06-08 22:43:58 +0900

    Update

--- resources/js/components/BokFlow/index.tsx
+++ resources/js/components/BokFlow/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
 import * as ResourceTypes from '../../resource-types';
 import { store } from '../../store';
 import { fetchBokFlow } from '../../actions';
@@ -10,7 +9,12 @@ import { isEmpty, getAuthUser } from '../../utils';
 import { Loading } from '../shared/Loading';
 import BokFlowContent from './BokFlowContent';
 
-class BokFlow extends React.Component {
+interface Props {
+    history: ResourceTypes.Route;
+    bokFlow?: Array<ResourceTypes.Bok>;
+}
+
+class BokFlow extends React.Component<Props> {
     componentDidMount() {
         if (!getAuthUser()) {
             return this.props.history.push('/login');
@@ -27,10 +31,5 @@ class BokFlow extends React.Component {
         return <BokFlowContent currentUser={currentUser} bokFlow={this.props.bokFlow} />;
     }
 }
-
-BokFlow.propTypes = {
-    history: ResourceTypes.ROUTER,
-    bokFlow: PropTypes.arrayOf(ResourceTypes.BOK),
-};
 
 export default withRouter(connect(state => state)(BokFlow));
