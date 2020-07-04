d61a9a56f5840c80bd301bc201eb31db70a8499a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2020-07-04 23:19:54 +0900

    OGPで本の画像を出せるようにUserBookの詳細だけ事前取得するコントローラーを追加

---
+++ app/Http/Controllers/Ssr/UserBookShowController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Ssr;
+
+use Illuminate\Http\Request;
+
+class UserBookShowController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
