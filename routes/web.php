b9445ed3af91b04f3daee435be2d6ae2e7a9fd87
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2020-07-05 23:50:58 +0900

    OGP対応したルーティングを1つ追加

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/auth/password/reset', function () {
     return view('layouts/app');
 })->name('dummy.auth.password.reset');
 
+Route::get('/users/{userId}/user_books/{userBookId}', 'Ssr\UserBookShowController')
+    ->where(['userId' => '[0-9]+', 'userBookId' => '[0-9]+'])
+    ->name('ogp.user_book');
 Route::get('/{url?}', function () {
     return view('layouts/app');
 })->where('url', '(.*)');
