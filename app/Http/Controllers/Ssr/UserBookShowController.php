7cac1632072a0adf85f85788364a8a12b129efd3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2020-07-05 23:50:38 +0900

    Add ogp controller

--- app/Http/Controllers/Ssr/UserBookShowController.php
+++ app/Http/Controllers/Ssr/UserBookShowController.php
@@ -2,18 +2,33 @@
 
 namespace App\Http\Controllers\Ssr;
 
-use Illuminate\Http\Request;
+use App\Models\UserBook;
+use App\Http\Controllers\Controller;
 
 class UserBookShowController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * UserBookの詳細ページへリンクされた時のOGPを設定するためのハック
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  userId: ユーザの主キー
+     * @param  userBookId: ユーザブックの主キー
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(int $userId, int $userBookId)
     {
-        //
+        $userBook = UserBook::query()
+            ->with([
+                'book:id,name,cover,description',
+                'user:id,name',
+            ])
+            ->find($userBookId);
+
+        $ogpTitle = $userBook->book->name . ':' . $userBook->user->name . 'さんの感想';
+        $ogpDescription = $ogpTitle . '、Boksページです。' . $userBook->book->description;
+        return view('layouts/app', [
+            'ogp_image' => $userBook->book->cover,
+            'ogp_title' => $ogpTitle,
+            'ogp_description' => $ogpDescription,
+        ]);
     }
 }
